8e59584fb3c04b2b633b79b7cb3ca0f6345e8ac9
Jonathan Johnson <jon.johnson@ucsf.edu>
2010-12-01 01:18:32 -0800

    The messages javascript class is more generalized and has a nice progress bar function.

--- app/common/scripts/messages.js
+++
@@ -1,7 +0,0 @@
-function Messages(){
-  var self = this;
-  
-  this.create = function(type, text){
-    $('#messages').append($('<p>').addClass(type).html(text).slideDown('slow').delay(10000).fadeOut('slow').slideUp('slow'));
-  }
-}
