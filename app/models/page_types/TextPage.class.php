4b17f169146e79fb47236830d56b62868488d16e
Jon Johnson <jon.johnson@ucsf.edu>
2010-12-01 01:02:55 -0800

    The ApplyPage modles no longer control the page editing process.  It will be built entireley in javascript.

--- app/models/page_types/TextPage.class.php
+++ app/models/page_types/TextPage.class.php
@@ -22,46 +22,6 @@ class TextPage extends StandardPage {
   }
   
   /**
-   * Edit the title and the sinle Text block
-   * @return Form
-   */
-  public function getEditPropertiesForm(){
-    $form = new Form;
-    $field = $form->newField(array('legend'=>"Edit {$this->applicationPage->title} properties"));
-    $element = $field->newElement('TextInput','title');
-    $element->label = 'Title';
-    $element->addValidator('NotEmpty');
-    $element->value = $this->applicationPage->title;
-    
-    $element = $field->newElement('Textarea','leadingText');
-    $element->label = 'Text';
-    $element->addValidator('NotEmpty');
-    $element->value = $this->applicationPage->leadingText;
-    
-    $form->newButton('submit', 'Save');
-    return $form;
-  }
-  
-  /**
-   * Remove the elemnts tab
-   * @return array
-   */
-  public function getTabs(){
-    $tabs = parent::getTabs();
-    unset($tabs['elements']);
-    return $tabs;
-  }
-  
-  /**
-   * Only has one text block which is stored in leadingText
-   */
-  public function editProperties(FormInput $input){
-    $this->applicationPage->Page->title = $input->title;
-    $this->applicationPage->Page->leadingText = $input->leadingText;
-    $this->applicationPage->save();
-  }
-  
-  /**
    * TextPages are always complete
    */
   public function getStatus(){
