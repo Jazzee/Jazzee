8e3f45d97f101e6d0414b97fb4185534ee12817d
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-01-27 15:10:03 -0800

    Add method to ApplicantsController to send applicants a notification email when their status changed.

--- app/classes/AdminControllers/ApplicantsController.class.php
+++ app/classes/AdminControllers/ApplicantsController.class.php
@@ -11,6 +11,22 @@ abstract class ApplicantsController extends AdminController{
     if($programID AND $cycleID AND $user)  return $user->isAllowed($controller, $action, $programID);
     return false;
   }
+  
+  /**
+   * Notify and applicant that their status has changed
+   * Several controllers (ApplicantsView, ApplicantsDecisions) can change an applicants status (decisions, sir, answer status)
+   * Use this method to send them notifications 
+   * @param Applicant $applicant
+   */
+  protected function notifyApplicantStatusUpdate(Applicant $applicant){
+    $mail = JazzeeMail::getInstance();
+    $message = new EmailMessage;
+    $message->to($applicant->email, "{$applicant->firstName} {$applicant->lastName}");
+    $message->from($applicant->Application->contactEmail, $applicant->Application->contactName);
+    $message->subject = 'Application Status';
+    $message->body = "We have updated your application status.  In order to protect your privacy you must login to see these changes.  " . $mail->path("apply/{$applicant->Application->Program->shortName}/{$applicant->Application->Cycle->name}/applicant/login");
+    $mail->send($message);
+  }
 }
 
 ?>
