6eddce2d5fcddc6cb4c866a0d99b53bb9ad9453e
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-01-27 15:25:58 -0800

    Add utility methods for setting and unestting the time stamps for each decision state.

--- app/models/doctrine/Decision.php
+++ app/models/doctrine/Decision.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Decision
  * 
@@ -8,9 +7,119 @@
  * @package    jazzee
  * @subpackage orm
  * @author     Jon Johnson <jon.johnson@ucsf.edu>
- * @license http://jazzee.org/license.txt
  */
-class Decision extends BaseDecision
-{
+class Decision extends BaseDecision{
+  
+  /**
+   * Format a decision time stamp
+   */
+  protected function decisionStamp(){
+    return date('Y-m-d H:i:s');
+  }
+  
+  /**
+   * Nominate for admit decision
+   */
+  public function nominateAdmit(){
+    if(!is_null($this->nominateDeny)){
+      throw new Jazzee_Exception('Cannot record two preliminary decisions');
+    }
+    if(is_null($this->nominateAdmit)) $this->nominateAdmit = $this->decisionStamp();
+  }
+  
+  /**
+   * Undo nominate admit
+   */
+  public function undoNominateAdmit(){
+    if(!is_null($this->finalAdmit)){
+      throw new Jazzee_Exception('Cannot remove a preliminary decision if a final decision has been recorded');
+    }
+    $this->nominateAdmit = null;
+  }
+  
+  /**
+   * Nominate for deny decision
+   */
+  public function nominateDeny(){
+    if(!is_null($this->nominateAdmit)){
+      throw new Jazzee_Exception('Cannot record two preliminary decisions');
+    }
+    if(is_null($this->nominateDeny)) $this->nominateDeny = $this->decisionStamp();
+  }
+  
+  /**
+   * Undo nominate deny
+   */
+  public function undoNominateDeny(){
+    if(!is_null($this->finalDeny)){
+      throw new Jazzee_Exception('Cannot remove a preliminary decision if a final decision has been recorded');
+    }
+    $this->nominateDeny = null;
+  }
+  
+  /**
+   * Final Deny Decision
+   */
+  public function finalDeny(){
+    if(!is_null($this->finalAdmit)){
+      throw new Jazzee_Exception('Cannot record two final decisions');
+    }
+    //if we don't have a preliminary decision record it now
+    if(is_null($this->nominateDeny)) $this->nominateDeny = $this->decisionStamp();
+    if(is_null($this->finalDeny)) $this->finalDeny = $this->decisionStamp();
+  }
+  
+  /**
+   * Undo Final Deny Decision
+   */
+  public function undoFinalDeny(){
+    $this->finalDeny = null;
+  }
+  
 
+  /**
+   * Final Admit Decision
+   */
+  public function finalAdmit(){
+    if(!is_null($this->finalDeny)){
+      throw new Jazzee_Exception('Cannot record two final decisions');
+    }
+    //if we don't have a preliminary decision record it now
+    if(is_null($this->nominateAdmit)) $this->nominateAdmit = $this->decisionStamp();
+    if(is_null($this->finalAdmit)) $this->finalAdmit = $this->decisionStamp();
+  }
+  
+  /**
+   * Undo Final Deny Decision
+   */
+  public function undoFinalAdmit(){
+    $this->finalAdmit = null;
+  }
+  
+  /**
+   * Accept Offer
+   */
+  public function acceptOffer(){
+    if(is_null($this->finalAdmit)){
+      throw new Jazzee_Exception('Cannot accept offer for an applicant who has not been admitted');
+    }
+    if(is_null($this->acceptOffer)) $this->acceptOffer = $this->decisionStamp();
+  }
+  
+  /**
+   * Decline Offer
+   */
+  public function declineOffer(){
+    if(is_null($this->finalAdmit)){
+      throw new Jazzee_Exception('Cannot decline offer for an applicant who has not been admitted');
+    }
+    if(is_null($this->declineOffer)) $this->declineOffer = $this->decisionStamp();
+  }
+  
+  /**
+   * Register a decision letter has been sent
+   */
+  public function decisionLetterSent(){
+    $this->decisionLetterSent = $this->decisionStamp();
+  }
 }
