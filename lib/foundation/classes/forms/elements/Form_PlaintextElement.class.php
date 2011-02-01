c03ca0b5a64eb6463758d47d0c1623ba3be17fe8
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-02-01 13:21:05 -0800

    Plaintext element for display plain text in a form field format.

---
+++ lib/foundation/classes/forms/elements/Form_PlaintextElement.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plaintext Element
+ * Displays plain text inside of a form
+ * @author Jon Johnson <jon.johnson@ucsf.edu>
+ * @package foundation
+ * @subpackage forms
+ */
+class Form_PlaintextElement extends Form_Element{}
+?>
---
+++ lib/foundation/classes/forms/lvc_elements/Plaintext.control.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plaintext element form control
+ * Display the value as text
+ * @author Jon Johnson <jon.johnson@ucsf.edu>
+ * @license http://jazzee.org/license.txt
+ * @package foundation
+ * @subpackage forms
+ */
+print $element->value;
