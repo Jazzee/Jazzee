2e6b29016a7e11020bd7d2f01663258568c36fc1
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-02-02 09:59:31 -0800

    Add path caching to Lvc views and controllers.

--- lib/foundation/classes/Lvc_FoundationConfig.class.php
+++ lib/foundation/classes/Lvc_FoundationConfig.class.php
@@ -6,6 +6,17 @@
  * @package foundation
  **/
 class Lvc_FoundationConfig extends Lvc_Config {
+  /**
+   *  Our cache
+   *  @var Cache 
+   */
+  static private $_cache;
+  
+  /**
+   * What we prefix our cached paths with
+   * @const string
+   */
+  const CACHE_PREFIX = 'FoundationLVCPath';
   
   /**
    * Get the path to a controller include
@@ -13,13 +24,41 @@ class Lvc_FoundationConfig extends Lvc_Config {
    * @return string path to controller
    */
   public static function getControllerPath($controllerName) {
+    if(!isset(self::$_cache)) self::$_cache = Cache::getInstance();
+    if($cachePath = self::$_cache->fetch(self::CACHE_PREFIX . 'Controller' . $controllerName)){
+      return $cachePath;
+    }
     foreach (self::$controllerPaths as $path) {
       $file = $path . $controllerName . self::$controllerSuffix;
       if (file_exists($file)) {
+        self::$_cache->store(self::CACHE_PREFIX . 'Controller' . $controllerName,$file);
         return $file;
       }
     }
     throw new Foundation_Exception("Path to {$controllerName} can not be found");
+  }
+  
+  /**
+   * Get the path to a view
+   * @param string $viewName
+   * @param string $viewType (Controller,Element,Layout)
+   * @param array $paths
+   * @param string $suffix
+   * @return string path to view file
+   */
+  public static function getViewPath($viewName, $viewType, $paths, $suffix) {
+    if(!isset(self::$_cache)) self::$_cache = Cache::getInstance();
+    if($cachePath = self::$_cache->fetch(self::CACHE_PREFIX . $viewType .'View' . $viewName)){
+      return $cachePath;
+    }
+    foreach ($paths as $path) {
+      $file = $path . $viewName . $suffix;
+      if (file_exists($file)) {
+        self::$_cache->store(self::CACHE_PREFIX . $viewType .'View' . $viewName,$file);
+        return $file;
+      }
+    }
+    throw new Foundation_Exception("Path to {$viewType} {$viewName} can not be found");
   }
   
   /**
@@ -31,16 +70,47 @@ class Lvc_FoundationConfig extends Lvc_Config {
     include_once($file);
   }
   
+	public static function getController($controllerName) {
+	  $file = self::getControllerPath($controllerName);
+		include_once($file);
+		$controllerClass = self::getControllerClassName($controllerName);
+		$controller = new $controllerClass();
+		$controller->setControllerName($controllerName);
+		return $controller;
+	}
+	
+  public static function getControllerView($viewName, &$data = array()) {
+    $file = self::getViewPath($viewName, 'controller', self::$controllerViewPaths, self::$controllerViewSuffix);
+    return new self::$viewClassName($file, $data);
+	}
+	
+	public static function getElementView($elementName, &$data = array()) {
+	  $file = self::getViewPath($viewName, 'element', self::$elementViewPaths, self::$elementViewSuffix);
+    return new self::$viewClassName($file, $data);
+	}
+	
+	public static function getLayoutView($layoutName, &$data = array()) {
+	  $file = self::getViewPath($viewName, 'layout', self::$layoutViewPaths, self::$layoutViewSuffix);
+    return new self::$viewClassName($file, $data);
+	}
+  
+  /**
+   * See if an element exists
+   * @param string $elementName
+   */
   public static function elementExists($elementName){
-    $exists = false;
+    if(!isset(self::$_cache)) self::$_cache = Cache::getInstance();
+    if($cachePath = self::$_cache->fetch(self::CACHE_PREFIX . 'Element' . $elementName)){
+      return true;
+    }
     foreach(self::$elementViewPaths as $path){
       $file = $path . $elementName . self::$elementViewSuffix;
       if (file_exists($file)) {
-        $exists = true;
-        break;
+        self::$_cache->store(self::CACHE_PREFIX . 'Element' . $elementName,$file);
+        return true;
       }
     }  
-    return $exists;
+    return false;
   }
   
   public static function prefixControllerPath($path) {
