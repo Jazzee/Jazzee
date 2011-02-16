22c807002ac680f645590b95cb7c0bf8800f36d8
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-02-16 14:59:22 -0800

    Base64 encode JazzeeConfig values so we can store anythign there. Update the config manager to call JazzeeConfig directly instead of caching values.

--- app/classes/ConfigManager.class.php
+++ app/classes/ConfigManager.class.php
@@ -4,43 +4,13 @@
  */
 class ConfigManager {
   /**
-   * All the current config variables
-   * @var array
-   */
-  protected $properties = array();
-  
-  /**
-   * Constructor
-   */
-  public function __construct(){
-    $this->fillProperties();
-  }
-  
-  /**
-   * Fill the $vars array with table values
-   */
-  protected function fillProperties(){
-    $items = Doctrine::getTable('JazzeeConfig')->findAll(Doctrine::HYDRATE_ARRAY);
-    foreach($items as $item){
-      $this->properties[$item['name']] = $item['value'];
-    }
-  }
-  
-  /**
    * Get a value
    * @param string $name
    * @return string|null if the property isn't set
    */
   public function __get($name){
-    if(array_key_exists($name, $this->properties)){
-      return $this->properties[$name];
-    }
-    $trace = debug_backtrace();
-    trigger_error(
-      'Undefined property : ' . $name .
-      ' in ' . $trace[0]['file'] .
-      ' on line ' . $trace[0]['line'],
-      E_USER_NOTICE);
+    $item = Doctrine::getTable('JazzeeConfig')->findOneByName($name);
+    if($item) return $item->value;
     return null;
   }
   
@@ -52,16 +22,13 @@ class ConfigManager {
   public function __set($name, $value){
     $item = Doctrine::getTable('JazzeeConfig')->findOneByName($name);
     if($item){
-      $item->value = (string)$value;
+      $item->value = $value;
     } else {
       $item = new JazzeeConfig;
       $item->name = $name;
-      $item->value = (string)$value;
+      $item->value = $value;
     }
     $item->save();
-    $this->fillProperties();
   }
-  
-    
 }
 ?>
