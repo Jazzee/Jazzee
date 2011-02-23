2ae960e7e72813a17af76150173faea52927657a
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-02-22 21:27:13 -0800

    Rename and move Status JS class.

---
+++ app/common/scripts/classes/Status.class.js
@@ -0,0 +1,25 @@
+function Status(canvas){
+  this.counter = 0;
+  this.canvas = canvas;
+  $(canvas).append($('<div>').attr('id', 'status-bar').append($('<img src="resource/common/media/ajax-bar.gif">').hide()));
+  $(canvas).append($('<div>').attr('id', 'status-message'));
+}
+
+Status.prototype.start = function(){
+  this.counter++;
+  this.updateBar();
+};
+
+Status.prototype.end = function(){
+  this.counter--;
+  this.updateBar();
+};
+
+Status.prototype.updateBar = function(){
+  if(this.counter > 0) $('#status-bar img').show();
+  if(this.counter == 0) $('#status-bar img').fadeOut(2000);
+};
+
+Status.prototype.addMessage = function(type, text){
+  $('#status-message').append($('<p>').addClass(type).html(text).slideDown('slow').delay(5000).fadeOut('slow').slideUp('slow'));
+};
