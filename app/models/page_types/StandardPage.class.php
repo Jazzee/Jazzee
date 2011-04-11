b65d4136986156ea6d6c36106c184f20a466f155
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-04-11 08:25:55 -0700

    Call ApplyPage::setActionPath when overriding it in StandardPage.

--- app/models/page_types/StandardPage.class.php
+++ app/models/page_types/StandardPage.class.php
@@ -14,6 +14,7 @@ class StandardPage extends ApplyPage {
    * @see ApplyPage::setActionPath()
    */
   public function setActionPath($actionPath){
+    parent::setActionPath($actionPath);
     $this->form->action = $actionPath;
   }
   
