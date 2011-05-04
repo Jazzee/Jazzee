2e140d5722842159194c41e1d98cb5a649bf0191
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-05-04 09:46:38 -0700

    AdminCron gets its own model to track cron objects.

--- app/controllers/admin/admin_cron.php
+++ app/controllers/admin/admin_cron.php
@@ -7,31 +7,74 @@
  * @subpackage admin
  */
 class AdminCronController extends AdminController {
+  /**
+   * The maximum time a cron semephor can stick around before throwing an exception and deleting itself
+   */
+  const MAX_INTERVAL = 86400;
+  
   public function actionIndex(){
     $startTime = time();
-    $lastRun = $this->getVariable('cron_lastRun');
-    $semaphore = $this->getVariable('cron_semaphore');
-    //dont run more often than every 30 minutes
-    //dont ever run two jobs at once
-    if(($lastRun and $startTime - $lastRun < 1800) or $semaphore){
-      die('no run');
+    if(!$this->semaphore()){
       exit(1);
     }
-    $this->setVariable('cron_semaphore', 'true');
-    $this->setVariable('cron_lastRun', $startTime);
     foreach($this->listControllers() as $className){
-      Lvc_FoundationConfig::includeController($className);
-      $class = Lvc_Config::getControllerClassName($className);
-      if(call_user_func(array($class, 'runCron'),$this->getVariable("{$className}_cron_lastRun"))){
-        $this->setVariable("{$className}_cron_lastRun", time());
+      FoundationVC_Config::includeController($className);
+      $class = FoundationVC_Config::getControllerClassName($className);
+      if(call_user_func(array($class, 'runCron'),$this->get("{$className}lastRun"))){
+        $this->set("{$className}lastRun", time());
       }
     }
+    //clear the semephore
+    $this->set('adminCronSemephore', false);
     //cron outputs nothing
     exit(0);
 	}
   
   public static function isAllowed($controller, $action, $user, $programID, $cycleID, $actionParams){
-    return true; //everyone is allowed to use cron
+    return true; //anyone is allowed to use cron
+  }
+  
+/**
+   * Get value from the cron store
+   * @param string $name
+   */
+  protected function get($name){
+    if($cron = Doctrine::getTable('Cron')->findOneByName($name)){
+      return $cron->value;
+    }
+    return false;
+  }
+  
+  /**
+   * Set value to the cron store
+   * @param string $name
+   * @param mixed $value
+   */
+  protected function set($name, $value){
+    if(!$cron = Doctrine::getTable('Cron')->findOneByName($name)){
+      $cron = new Cron;
+      $cron->name = $name;
+    }
+    $cron->value = $value;
+    $cron->save();
+    return true;
+  }
+  
+  protected function semaphore(){
+    $semephore = $this->get('adminCronSemephore');
+    $lastRun = $this->get('adminCronLastRun');
+    if(!empty($semephore)){
+      //attempting to run cron again too soon
+      if(time() - (int)$lastRun < self::MAX_INTERVAL) return false;
+      
+      //This semephore has been around for too long delete it and throw an exception so the next run can proceed normally
+      $this->set('adminCronSemephore', false);
+      throw new Jazzee_Exception('AdminCron semephore was set last at ' . date('r',$lastRun) .  ' which was more than ' . self::MAX_INTERVAL . ' seconds ago.  This can indicate a broken cron process.', E_ERROR);
+    }
+    $this->set('adminCronSemephore', true);
+    $this->set('adminCronLastRun', time());
+    
+    return true;
   }
 }
 
