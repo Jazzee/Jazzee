27a6206a948a22819cb52426bbe59317abb60c4d
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-05-25 13:20:09 -0700

    Refactor Controller constructor to move each setup into its own method for clarity

--- src/Jazzee/Controller.php
+++ src/Jazzee/Controller.php
@@ -59,151 +59,12 @@ class Controller extends \Foundation\VC\Controller
    * Setup error processing and email
    */
   public function __construct(){
-    //load the configuration and cache it
-    $this->_config = new \Jazzee\Configuration();
-    
-    $this->_foundationConfig = new \Foundation\Configuration();
-    if($this->_config->getStatus() == 'DEVELOPMENT'){
-      $this->_foundationConfig->setCacheType('array');
-    } else {
-      $this->_foundationConfig->setCacheType('apc');
-    }
-    $this->_foundationConfig->setMailSubjectPrefix($this->_config->getMailSubjectPrefix());
-    $this->_foundationConfig->setMailDefaultFromAddress($this->_config->getMailDefaultFrom());
-    
-    $this->_cache = new \Foundation\Cache('Jazzee',$this->_foundationConfig);
-    
-    \Foundation\VC\Config::setCache($this->_cache);
-    
-    if((empty($_SERVER['HTTPS']) OR $_SERVER['HTTPS'] == 'off') AND !$this->_config->getForceSSL()){
-      $protocol = 'http';
-    } else {
-      $protocol = 'https';
-    }
-    
-    $this->_serverPath = $protocol . '://' .  $_SERVER['SERVER_NAME'];
-    
-    //set the default timezone
-    date_default_timezone_set($this->_config->getTimezone());
-    
-    $var = $this->getVarPath();
-    
-    if(!\is_dir($var) or !\is_writable($var)){
-      throw new Exception("{$var} is not readable by the webserver so we cannot use it as the 'var' directory");
-    }
-    
-    //check to see if all the directories exist and are writable
-    $varDirectories = array('log','session','tmp','uploads');
-    foreach($varDirectories as $dir){
-      $path = $var . '/' . $dir;
-      if(!is_dir($path)){
-        if(!mkdir($path)){
-          throw new Exception("Tried to create 'var/{$dir}' directory but {$path} is not writable by the webserver");
-        }
-      }
-      if(!is_writable($path)){
-        throw new Exception("Invalid path to 'var/{$dir}' {$path} is not writable by the webserver");
-      }
-    }
-    
-    //setup doctrine
-    $doctrineConfig = new \Doctrine\ORM\Configuration();
-
-    //We use different caching and proxy settings in Development status
-    if($this->_config->getStatus() == 'DEVELOPMENT'){
-      $doctrineConfig->setAutoGenerateProxyClasses(true);
-      $doctrineConfig->setProxyDir($var . '/tmp');
-      $cache = new \Doctrine\Common\Cache\ArrayCache;
-    } else {
-      $doctrineConfig->setAutoGenerateProxyClasses(false);
-      $doctrineConfig->setProxyDir(__DIR__ . '/Entity/Proxy');
-      if(!extension_loaded('apc')) throw new Exception('APC cache is required, but was not available.');
-      $cache = new \Doctrine\Common\Cache\ApcCache;
-    }
-    $driver = $doctrineConfig->newDefaultAnnotationDriver(array(__DIR__."/Entity"));
-    $doctrineConfig->setMetadataDriverImpl($driver);
-    
-    $doctrineConfig->setProxyNamespace('Entity\Proxy');
-    $doctrineConfig->setMetadataCacheImpl($cache);
-    $doctrineConfig->setQueryCacheImpl($cache);
-    
-    $connectionParams = array(
-      'dbname' => $this->_config->getDbName(),
-      'user' => $this->_config->getDbUser(),
-      'password' => $this->_config->getDbPassword(),
-      'host' => $this->_config->getDbHost(),
-      'port' => $this->_config->getDbPort(),
-      'driver' => $this->_config->getDbDriver(),
-    );
-    
-    $this->_em = \Doctrine\ORM\EntityManager::create($connectionParams, $doctrineConfig);
-   
-    //setup the session based on the configuration
-    $this->_session = new \Foundation\Session();
-    
-    //if the session name variable is empty then there is no way to login and fix it so look for an empty session name and default to the ini value if it is blank
-    $this->_session->setConfigVariable('name', $this->_config->getSessionName()?ini_get('session.name'):$this->_config->getSessionName());
-    //cookies last forever (until browser is closed) which takes the users local clock out of the picture
-    //Timeouts are handled By Session internally by expiring the Session_Store
-    $this->_session->setConfigVariable('cookie_lifetime', 0);
-    //since files are stored in sessions destroy any files after one day
-    $this->_session->setConfigVariable('gc_maxlifetime', 86400);
-    $this->_session->setConfigVariable('use_only_cookies', true);
-    $this->_session->setConfigVariable('hash_function', 1);
-    $this->_session->setConfigVariable('save_path', $var . '/session/');
-    if($this->_config->getForceSSL() OR (!empty($_SERVER['HTTPS']) AND $_SERVER['HTTPS'] == 'on')){
-      $this->_session->setConfigVariable('cookie_secure', true);
-    }
-    $this->_session->setConfigVariable('cookie_path', rtrim(dirname($_SERVER['SCRIPT_NAME']),'/\\.') . '/');
-    //browsers give inconsisten results when the domain is used to set the cookie, instead use an empty string to restrict the cookie to this domain
-    $this->_session->setConfigVariable('cookie_domain', '');
-    $this->_session->start();
-    
+    $this->setupConfig();
+    $this->setupVarPath();
+    $this->setupDoctrine(); 
+    $this->setupSession();
     $this->buildVirtualFilesystem();
-    /*
-    //create an access log with browser information
-    $accessLog = Log::singleton('file', VAR_ROOT . '/log/access_log', '', array('lineFormat'=>'%{timestamp} %{message}'),PEAR_LOG_INFO);
-    $accessMessage ="[{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']} {$_SERVER['SERVER_PROTOCOL']}] " .
-      '[' . (!empty($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'-') . '] ' .
-      '[' . (!empty($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']:'-') . ']';
-    $accessLog->log($accessMessage, PEAR_LOG_INFO);
-    
-    //Add an observer to catch all generated errors and put them in the error_log
-    Error::getInstance()->attach(
-      new PearLogObserver(
-        Log::singleton('file',VAR_ROOT . '/log/error_log')
-      ),E_ALL ^ (E_NOTICE | E_USER_NOTICE)
-    );
-    
-    //special log for Strict errors which are the results of depriciation mostly
-    Error::getInstance()->attach(
-      new PearLogObserver(
-        Log::singleton('file',VAR_ROOT . '/log/strict_log')
-      ),E_DEPRECATED | E_USER_DEPRECATED | E_STRICT | E_NOTICE | E_USER_NOTICE
-    );
-    
-    //Also direct all errors out to PHP error_log function where the belong
-    Error::getInstance()->attach(
-      new PearLogObserver(
-        Log::factory('error_log',PEAR_LOG_TYPE_SYSTEM,'Jazzee Error')
-      ),E_ALL
-    );
-    
-    //In developemnt log errors to screen
-    if($this->config->status == 'DEVELOPMENT'){
-      Error::getInstance()->attach(
-        new PearLogObserver(
-          Log::factory('display','','', array(),PEAR_LOG_DEBUG)
-        ),E_ALL
-      );
-    }
-    
-    //Set Error to be the default error handler
-    set_error_handler(array(Error::getInstance(), 'register'));
-    
-    //set Error to handle uncaught exceptions
-    set_exception_handler(array(Error::getInstance(), 'exception'));
-    */
+    //$this->setupLogging();
   }
   
   /**
@@ -319,6 +180,129 @@ class Controller extends \Foundation\VC\Controller
   }
   
   /**
+   * Setup configuration
+   * 
+   * Load config.ini.php
+   * translate to foundation config
+   * create absolute path
+   * set defautl timezone
+   */
+  protected function setupConfiguration(){
+    $this->_config = new \Jazzee\Configuration();
+    
+    $this->_foundationConfig = new \Foundation\Configuration();
+    if($this->_config->getStatus() == 'DEVELOPMENT'){
+      $this->_foundationConfig->setCacheType('array');
+    } else {
+      $this->_foundationConfig->setCacheType('apc');
+    }
+    $this->_foundationConfig->setMailSubjectPrefix($this->_config->getMailSubjectPrefix());
+    $this->_foundationConfig->setMailDefaultFromAddress($this->_config->getMailDefaultFrom());
+    
+    $this->_cache = new \Foundation\Cache('Jazzee',$this->_foundationConfig);
+    
+    \Foundation\VC\Config::setCache($this->_cache);
+    
+    if((empty($_SERVER['HTTPS']) OR $_SERVER['HTTPS'] == 'off') AND !$this->_config->getForceSSL()){
+      $protocol = 'http';
+    } else {
+      $protocol = 'https';
+    }
+    
+    $this->_serverPath = $protocol . '://' .  $_SERVER['SERVER_NAME'];
+    
+    //set the default timezone
+    date_default_timezone_set($this->_config->getTimezone());
+  }
+  
+  /**
+   * Setup Doctrine ORM
+   */
+  protected function setupDoctrine(){
+    //setup doctrine
+    $doctrineConfig = new \Doctrine\ORM\Configuration();
+
+    //We use different caching and proxy settings in Development status
+    if($this->_config->getStatus() == 'DEVELOPMENT'){
+      $doctrineConfig->setAutoGenerateProxyClasses(true);
+      $doctrineConfig->setProxyDir($this->getVarPath() . '/tmp');
+      $cache = new \Doctrine\Common\Cache\ArrayCache;
+    } else {
+      $doctrineConfig->setAutoGenerateProxyClasses(false);
+      $doctrineConfig->setProxyDir(__DIR__ . '/Entity/Proxy');
+      if(!extension_loaded('apc')) throw new Exception('APC cache is required, but was not available.');
+      $cache = new \Doctrine\Common\Cache\ApcCache;
+    }
+    $driver = $doctrineConfig->newDefaultAnnotationDriver(array(__DIR__."/Entity"));
+    $doctrineConfig->setMetadataDriverImpl($driver);
+    
+    $doctrineConfig->setProxyNamespace('Entity\Proxy');
+    $doctrineConfig->setMetadataCacheImpl($cache);
+    $doctrineConfig->setQueryCacheImpl($cache);
+    
+    $connectionParams = array(
+      'dbname' => $this->_config->getDbName(),
+      'user' => $this->_config->getDbUser(),
+      'password' => $this->_config->getDbPassword(),
+      'host' => $this->_config->getDbHost(),
+      'port' => $this->_config->getDbPort(),
+      'driver' => $this->_config->getDbDriver(),
+    );
+    
+    $this->_em = \Doctrine\ORM\EntityManager::create($connectionParams, $doctrineConfig);
+  }
+  
+  /**
+   * Setup the var directories
+   */
+  protected function setupVarPath(){
+    $var = $this->getVarPath();
+    if(!\is_dir($var) or !\is_writable($var)){
+      throw new Exception("{$var} is not readable by the webserver so we cannot use it as the 'var' directory");
+    }
+    
+    //check to see if all the directories exist and are writable
+    $varDirectories = array('log','session','tmp','uploads');
+    foreach($varDirectories as $dir){
+      $path = $var . '/' . $dir;
+      if(!is_dir($path)){
+        if(!mkdir($path)){
+          throw new Exception("Tried to create 'var/{$dir}' directory but {$path} is not writable by the webserver");
+        }
+      }
+      if(!is_writable($path)){
+        throw new Exception("Invalid path to 'var/{$dir}' {$path} is not writable by the webserver");
+      }
+    }
+  }
+  
+  /**
+   * Setup Sessions
+   */
+  protected function setupSession(){
+    //setup the session based on the configuration
+    $this->_session = new \Foundation\Session();
+    
+    //if the session name variable is empty then there is no way to login and fix it so look for an empty session name and default to the ini value if it is blank
+    $this->_session->setConfigVariable('name', $this->_config->getSessionName()?ini_get('session.name'):$this->_config->getSessionName());
+    //cookies last forever (until browser is closed) which takes the users local clock out of the picture
+    //Timeouts are handled By Session internally by expiring the Session_Store
+    $this->_session->setConfigVariable('cookie_lifetime', 0);
+    //since files are stored in sessions destroy any files after one day
+    $this->_session->setConfigVariable('gc_maxlifetime', 86400);
+    $this->_session->setConfigVariable('use_only_cookies', true);
+    $this->_session->setConfigVariable('hash_function', 1);
+    $this->_session->setConfigVariable('save_path', $this->getVarPath() . '/session/');
+    if($this->_config->getForceSSL() OR (!empty($_SERVER['HTTPS']) AND $_SERVER['HTTPS'] == 'on')){
+      $this->_session->setConfigVariable('cookie_secure', true);
+    }
+    $this->_session->setConfigVariable('cookie_path', rtrim(dirname($_SERVER['SCRIPT_NAME']),'/\\.') . '/');
+    //browsers give inconsisten results when the domain is used to set the cookie, instead use an empty string to restrict the cookie to this domain
+    $this->_session->setConfigVariable('cookie_domain', '');
+    $this->_session->start();
+  }
+  
+  /**
    * Build our virtual file system
    */
   protected function buildVirtualFileSystem(){
@@ -351,5 +335,53 @@ class Controller extends \Foundation\VC\Controller
     $virtualFoundation->addDirectory('styles',$styles);
     $this->_vfs->addDirectory('foundation', $virtualFoundation);
 
+  }
+  
+  /**
+   * Setup logging
+   */
+  protected function setupLoggind(){
+    //create an access log with browser information
+    $accessLog = Log::singleton('file', VAR_ROOT . '/log/access_log', '', array('lineFormat'=>'%{timestamp} %{message}'),PEAR_LOG_INFO);
+    $accessMessage ="[{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']} {$_SERVER['SERVER_PROTOCOL']}] " .
+      '[' . (!empty($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'-') . '] ' .
+      '[' . (!empty($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']:'-') . ']';
+    $accessLog->log($accessMessage, PEAR_LOG_INFO);
+    
+    //Add an observer to catch all generated errors and put them in the error_log
+    Error::getInstance()->attach(
+      new PearLogObserver(
+        Log::singleton('file',VAR_ROOT . '/log/error_log')
+      ),E_ALL ^ (E_NOTICE | E_USER_NOTICE)
+    );
+    
+    //special log for Strict errors which are the results of depriciation mostly
+    Error::getInstance()->attach(
+      new PearLogObserver(
+        Log::singleton('file',VAR_ROOT . '/log/strict_log')
+      ),E_DEPRECATED | E_USER_DEPRECATED | E_STRICT | E_NOTICE | E_USER_NOTICE
+    );
+    
+    //Also direct all errors out to PHP error_log function where the belong
+    Error::getInstance()->attach(
+      new PearLogObserver(
+        Log::factory('error_log',PEAR_LOG_TYPE_SYSTEM,'Jazzee Error')
+      ),E_ALL
+    );
+    
+    //In developemnt log errors to screen
+    if($this->config->status == 'DEVELOPMENT'){
+      Error::getInstance()->attach(
+        new PearLogObserver(
+          Log::factory('display','','', array(),PEAR_LOG_DEBUG)
+        ),E_ALL
+      );
+    }
+    
+    //Set Error to be the default error handler
+    set_error_handler(array(Error::getInstance(), 'register'));
+    
+    //set Error to handle uncaught exceptions
+    set_exception_handler(array(Error::getInstance(), 'exception'));
   }
 }
