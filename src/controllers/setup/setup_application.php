1e87493ca28097083098bbd24cd53a6d6e9c8b96
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-05-29 23:36:39 -0700

    setup application working

--- src/controllers/setup/setup_application.php
+++ src/controllers/setup/setup_application.php
@@ -11,15 +11,17 @@ class SetupApplicationController extends \Jazzee\AdminController {
   const TITLE = 'Application';
   const PATH = 'setup/application';
   
+  const ACTION_INDEX = 'Make Changes';
+  
   /**
    * If there is no application then create a new one to work with
    */
   protected function setUp(){
     parent::setUp();
-    if($this->application === false){
-      $this->application = new Application;
-      $this->application->programID = $this->program->id;
-      $this->application->cycleID = $this->cycle->id;
+    if(!$this->_application){
+      $this->_application = new \Jazzee\Entity\Application();
+      $this->_application->setProgram($this->_program);
+      $this->_application->setCycle($this->_cycle);
     }
   }
   
@@ -27,92 +29,70 @@ class SetupApplicationController extends \Jazzee\AdminController {
    * Setup the current application and cycle
    */
   public function actionIndex(){
-    $form = new Form;
-    $form->action = $this->path("setup/application");
-    $field = $form->newField(array('legend'=>'Setup Application'));
+    $form = new \Foundation\Form();
+    $form->setAction($this->path("setup/application"));
+    $field = $form->newField();
+    $field->setLegend('Setup Applicant');
     
     $element = $field->newElement('TextInput','contactName');
-    $element->label = 'Contact Name';
-    $element->addValidator('NotEmpty');
-    $element->value = $this->application->contactName;
+    $element->setLabel('Contact Name');
+    $element->addValidator(new \Foundation\Form\Validator\NotEmpty($element));
+    $element->setValue($this->_application->getContactName());
     
     $element = $field->newElement('TextInput','contactEmail');
-    $element->label = 'Contact Email';
-    $element->addValidator('NotEmpty');
-    $element->addValidator('EmailAddress');
-    $element->value = $this->application->contactEmail;
-    
-    $element = $field->newElement('TextInput','contactPhone');
-    $element->label = 'Contact Phone';
-    $element->value = $this->application->contactPhone;
+    $element->setLabel('Contact Email');
+    $element->addValidator(new \Foundation\Form\Validator\NotEmpty($element));
+    $element->addValidator(new \Foundation\Form\Validator\EmailAddress($element));
+    $element->setValue($this->_application->getContactEmail());
     
     $element = $field->newElement('Textarea','welcome');
-    $element->label = 'Welcome Message';
-    $element->value = $this->application->welcome;
+    $element->setLabel('Welcome Message');
+    $element->setValue($this->_application->getWelcome());
     
-    $element = $field->newElement('TextInput','open');
-    $element->label = 'Application Open';
-    $element->addValidator('Date');
-    $element->addValidator('DateBeforeElement', 'close');
-    $element->addFilter('DateFormat', 'Y-m-d H:i:s');
-      $element->value = $this->application->open;
+    $element = $field->newElement('DateInput','open');
+    $element->setLabel('Application Open');
+    $element->addValidator(new \Foundation\Form\Validator\DateBeforeElement($element, 'close'));
+    $element->setValue($this->_application->getOpen()->format('c'));
     
-    $element = $field->newElement('TextInput','close');
-    $element->label = 'Application Deadline';
-    $element->addValidator('Date');
-    $element->addFilter('DateFormat', 'Y-m-d H:i:s');
-    $element->value = $this->application->close;
-    
-    $element = $field->newElement('TextInput','begin');
-    $element->label = 'Program Start Date';
-    $element->addValidator('Date');
-    $element->addFilter('DateFormat', 'Y-m-d H:i:s');
-    $element->addValidator('NotEmpty');
-    $element->value = $this->application->begin;
+    $element = $field->newElement('DateInput','close');
+    $element->setLabel('Application close');
+    $element->setValue($this->_application->getClose()->format('c'));
+
+    $element = $field->newElement('DateInput','begin');
+    $element->setLabel('Program Start Date');
+    $element->setValue($this->_application->getBegin()->format('c'));
     
     $element = $field->newElement('RadioList','visible');
-    $element->label = 'Visible';
-    $element->addItem(0, 'No');
-    $element->addItem(1, 'Yes');
-    $element->addValidator('NotEmpty');
-    $element->value = $this->application->visible;
+    $element->setLabel('Visible');
+    $element->newItem(0, 'No');
+    $element->newItem(1, 'Yes');
+    $element->addValidator(new \Foundation\Form\Validator\NotEmpty($element));
+    $element->setValue($this->_application->isVisible());
     
     $element = $field->newElement('RadioList','published');
-    $element->label = 'Published';
-    $element->addItem(0, 'No');
-    $element->addItem(1, 'Yes');
-    $element->addValidator('NotEmpty');
-    $element->value = $this->application->published;
+    $element->setLabel('Published');
+    $element->newItem(0, 'No');
+    $element->newItem(1, 'Yes');
+    $element->addValidator(new \Foundation\Form\Validator\NotEmpty($element));
+    $element->setValue($this->_application->isPublished());
 
     $form->newButton('submit', 'Save');
     
     if($input = $form->processInput($this->post)){
-      $this->application->contactName = $input->contactName;
-      $this->application->contactEmail = $input->contactEmail;
-      $this->application->contactPhone = $input->contactPhone;
-      $this->application->welcome = $input->welcome;
-      $this->application->open = $input->open;
-      $this->application->close = $input->close;
-      $this->application->begin = $input->begin;
-      $this->application->feeDomestic = $input->feeDomestic;
-      $this->application->feeForeign = $input->feeForeign;
-      $this->application->visible = (bool)$input->visible;
-      $this->application->published = (bool)$input->published;
-      $this->application->save();
-      $this->messages->write('success', 'Application saved.');
-      $this->redirect($this->path("setup/application"));
-      $this->afterAction();
-      exit();
+      $this->_application->setContactName($input->get('contactName'));
+      $this->_application->setContactEmail($input->get('contactEmail'));
+      $this->_application->setWelcome($input->get('welcome'));
+      $this->_application->setOpen($input->get('open'));
+      $this->_application->setClose($input->get('close'));
+      $this->_application->setBegin($input->get('begin'));
+      if($input->get('visible')) $this->_application->visible(); else $this->_application->inVisible();
+      if($input->get('published')) $this->_application->publish(); else $this->_application->unPublish();
+      $this->_em->persist($this->_application);
+      $this->addMessage('success', 'Application saved.');
+      $this->redirectPath('setup/application');
     }
     
     $this->setVar('form', $form);
-  }
-  
-  public static function getControllerAuth(){
-    $auth = new ControllerAuth;
-    $auth->name = 'Setup Application';
-    $auth->addAction('index', new ActionAuth('Make Changes'));
-    return $auth;
   }
   
 }
