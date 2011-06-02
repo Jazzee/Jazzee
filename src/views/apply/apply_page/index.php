5af80d5f10d34d11185a190de31937c378d3669a
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-01 20:34:02 -0700

    add extendible counter for pages

--- src/views/apply/apply_page/index.php
+++ src/views/apply/apply_page/index.php
@@ -6,6 +6,8 @@
  * @package jazzee
  * @subpackage apply
  */
+?>
+<?php
 if($answers = $page->getJazzeePage()->getAnswers()){
   print "<div id='answers'>";
   $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-answer');
@@ -13,6 +15,15 @@ if($answers = $page->getJazzeePage()->getAnswers()){
     $this->renderElement($elementName, array('answer'=>$answer, 'page'=>$page,'currentAnswerID'=>$currentAnswerID));
   }
   print '</div>';
+}?>
+<div id='counter'><?php 
+  if($page->getJazzeePage()->getAnswers()){
+    $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-counter');
+    $this->renderElement($elementName, array('page'=>$page));
+  }?>
+</div>
+<?php
+if(is_null($page->getMax()) or count($page->getJazzeePage()->getAnswers()) < $page->getMax()){
+  $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-form');
+  $this->renderElement($elementName, array('page'=>$page));
 }
-$elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-form');
-$this->renderElement($elementName, array('page'=>$page));
