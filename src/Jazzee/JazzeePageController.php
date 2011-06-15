e074401af7f212c20b18756b0906fcbe31027aac
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-15 15:49:17 -0700

    move more functionality into JazzeePageController

--- src/Jazzee/JazzeePageController.php
+++ src/Jazzee/JazzeePageController.php
@@ -10,11 +10,37 @@ namespace Jazzee;
 class JazzeePageController extends \Foundation\VC\Controller
 { 
   /**
+   *  @var \Jazzee\Configuration
+   */
+  protected $_config;
+  
+  /**
+   *  @var \Foundation\Configuration
+   */
+  protected $_foundationConfig;
+  
+  /**
+   * @var \Foundation\Cache
+   */
+  protected $_cache;
+  
+  /**
+   * Absolute server path
+   * @var string
+   */
+  protected $_serverPath;
+  
+  /**
    * Virtual File system root directory
    * @var \Foundation\Virtual\Directory
    */
   protected $_vfs;
   
+  public function __construct(){
+    $this->setupConfiguration();
+    $this->setupVarPath();
+    $this->setupLogging();
+  }
   /**
    * Basic page disply setup
    * 
@@ -45,13 +71,23 @@ class JazzeePageController extends \Foundation\VC\Controller
   }
   
   /**
-   * Fall back on a relative path with no pretty urls
-   * 
+   * Create a good path even if modrewrite is not present
    * @param string $path
    * @return string
    */
   public function path($path){
-    return './index.php?url=' . $path;
+    $prefix = $this->_serverPath . rtrim(dirname($_SERVER['SCRIPT_NAME']),'/\\.');
+    return $prefix . '/' . $path;
+  }
+
+  /**
+   * Call any after action properties, redirect, and exit
+   * @param string $path
+   */
+  public function redirectPath($path){
+    $this->redirect($this->path($path));
+    $this->afterAction();
+    exit(0);
   }
   
   /**
@@ -101,5 +137,194 @@ class JazzeePageController extends \Foundation\VC\Controller
    */
   public function getNavigation(){
     return false;
+  }
+  
+  /**
+   * Setup the var directories
+   */
+  protected function setupVarPath(){
+    $var = $this->getVarPath();
+    //check to see if all the directories exist and are writable
+    $varDirectories = array('log','session','tmp','uploads');
+    foreach($varDirectories as $dir){
+      $path = $var . '/' . $dir;
+      if(!is_dir($path)){
+        if(!mkdir($path)){
+          throw new Exception("Tried to create 'var/{$dir}' directory but {$path} is not writable by the webserver");
+        }
+      }
+      if(!is_writable($path)){
+        throw new Exception("Invalid path to 'var/{$dir}' {$path} is not writable by the webserver");
+      }
+    }
+  }
+  
+  /**
+   * Get the path to the var directory
+   * @return string
+   */
+  protected function getVarPath(){
+    $path = $this->_config->getVarPath()?$this->_config->getVarPath():__DIR__ . '/../../var';
+    if(!$realPath = \realpath($path) or !\is_dir($realPath) or !\is_writable($realPath)){
+      if($realPath) $path = $realPath; //nicer error message if the path exists
+      throw new Exception("{$path} is not readable by the webserver so we cannot use it as the 'var' directory");
+    }
+    return $realPath;
+  }
+  
+  /**
+   * Setup configuration
+   * 
+   * Load config.ini.php
+   * translate to foundation config
+   * create absolute path
+   * set defautl timezone
+   */
+  protected function setupConfiguration(){
+    $this->_config = new \Jazzee\Configuration();
+    
+    $this->_foundationConfig = new \Foundation\Configuration();
+    if($this->_config->getStatus() == 'DEVELOPMENT'){
+      $this->_foundationConfig->setCacheType('array');
+    } else {
+      $this->_foundationConfig->setCacheType('apc');
+    }
+    $this->_foundationConfig->setMailSubjectPrefix($this->_config->getMailSubjectPrefix());
+    $this->_foundationConfig->setMailDefaultFromAddress($this->_config->getMailDefaultFromAddress());
+    $this->_foundationConfig->setMailDefaultFromName($this->_config->getMailDefaultFromName());
+    $this->_foundationConfig->setMailOverrideToAddress($this->_config->getMailOverrideToAddress());
+    $this->_foundationConfig->setMailServerType($this->_config->getMailServerType());
+    $this->_foundationConfig->setMailServerHost($this->_config->getMailServeHostr());
+    $this->_foundationConfig->setMailServerPort($this->_config->getMailServerPort());
+    $this->_foundationConfig->setMailServerUsername($this->_config->getMailServerUsername());
+    $this->_foundationConfig->setMailServerPassword($this->_config->getMailServerPassword());
+    
+    
+    $this->_cache = new \Foundation\Cache('Jazzee',$this->_foundationConfig);
+    
+    \Foundation\VC\Config::setCache($this->_cache);
+    
+    if((empty($_SERVER['HTTPS']) OR $_SERVER['HTTPS'] == 'off')){
+      $protocol = 'http';
+    } else {
+      $protocol = 'https';
+    }
+    
+    $this->_serverPath = $protocol . '://' .  $_SERVER['SERVER_NAME'];
+  }
+  
+  /**
+   * Setup logging
+   */
+  protected function setupLogging(){
+    $path = $this->getVarPath() . '/log';
+    //create an access log with browser information
+    $accessLog = \Log::singleton('file', $path . '/access_log', '', array('lineFormat'=>'%{timestamp} %{message}'),PEAR_LOG_INFO);
+    $accessMessage ="[{$_SERVER['REQUEST_METHOD']} {$_SERVER['REQUEST_URI']} {$_SERVER['SERVER_PROTOCOL']}] " .
+      '[' . (!empty($_SERVER['HTTP_REFERER'])?$_SERVER['HTTP_REFERER']:'-') . '] ' .
+      '[' . (!empty($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']:'-') . ']';
+    $accessLog->log($accessMessage, PEAR_LOG_INFO);
+    
+    
+    $log = \Log::singleton('file', $path . '/error_log', '',array(), PEAR_LOG_ERR);
+    $strict = \Log::singleton('file', $path . '/strict_log');
+    $php = \Log::singleton('error_log', PEAR_LOG_TYPE_SYSTEM, 'Jazzee Error');
+    $this->_log = \Log::singleton('composite');
+    $this->_log->addChild($log);
+    $this->_log->addChild($strict);
+    $this->_log->addChild($php);
+    
+    //Handle PHP errors with out logs
+    set_error_handler(array($this, 'handleError'));
+    //catch any excpetions
+    set_exception_handler(array($this, 'handleException'));
+  }
+  
+  /**
+   * Handle PHP error
+   * Takes input from PHPs built in error handler logs it  
+   * throws a jazzee exception to handle if the error reporting level is high enough
+   * @param $code
+   * @param $message
+   * @param $file
+   * @param $line
+   * @throws \Jazzee\Exception
+   */
+  public function handleError($code, $message, $file, $line){
+    /* Map the PHP error to a Log priority. */
+    switch ($code) {
+      case E_WARNING:
+      case E_USER_WARNING:
+        $priority = PEAR_LOG_WARNING;
+        break;
+      case E_NOTICE:
+      case E_USER_NOTICE:
+        $priority = PEAR_LOG_NOTICE;
+        break;
+      case E_ERROR:
+      case E_USER_ERROR:
+        $priority = PEAR_LOG_ERR;
+        break;
+      default:
+        $priority = PEAR_LOG_INFO;
+    }
+    $this->_log->log($message . ' in ' . $file . ' at line ' . $line, $priority);
+    throw new Exception('Jazzee caught a PHP error');
+  }
+  
+
+  
+  /**
+   * Handle PHP Exception
+   * @param Exception $e
+   */
+  public function handleException(\Exception $e){
+    $message = $e->getMessage();
+    $userMessage = 'Unspecified Technical Difficulties';
+    $error = 500;
+    if($e instanceof \Lvc_Exception){
+      $code = 404;
+      $userMessage = 'Page not found.';
+    }
+    if($e instanceof \PDOException){
+      $message = 'Problem with database connection. PDO says: ' . $message;
+      $userMessage = 'We are experiencing a problem connecting to our database.  Please try your request again.';
+    }
+    if($e instanceof \Foundation\Exception){
+      $userMessage = $e->getUserMessage();
+    }
+    if($e instanceof \Foundation\Virtual\Exception){
+      $userMessage = $e->getUserMessage();
+      $code = $e->getHttpErrorCode();
+    }
+    /* Map the PHP error to a Log priority. */
+    switch ($e->getCode()) {
+      case E_WARNING:
+      case E_USER_WARNING:
+        $priority = PEAR_LOG_WARNING;
+        break;
+      case E_NOTICE:
+      case E_USER_NOTICE:
+        $priority = PEAR_LOG_NOTICE;
+        break;
+      case E_ERROR:
+      case E_USER_ERROR:
+        $priority = PEAR_LOG_ERR;
+        break;
+      default:
+        $priority = PEAR_LOG_INFO;
+    }
+    $this->_log->log($message, $priority);
+    
+    // Get a request for the error page
+    $request = new \Lvc_Request();
+    $request->setControllerName('error');
+    $request->setActionName('index');
+    $request->setActionParams(array('error' => $error, 'message'=>$userMessage));
+  
+    // Get a new front controller without any routers, and have it process our handmade request.
+    $fc = new \Lvc_FrontController();
+    $fc->processRequest($request);
+    exit(1);
   }
 }
