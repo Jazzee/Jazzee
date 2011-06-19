6245939ffd579357a5d46606ee333b43734f11a0
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-19 14:01:01 -0700

    only show differnt option on branching subpages

--- src/views/page_type_elements/Branching-form.php
+++ src/views/page_type_elements/Branching-form.php
@@ -4,8 +4,10 @@
  * @package jazzee
  * @subpackage apply
  */
-?>
-<p><a href='<?php print $this->controller->getActionPath() ?>'>Choose a different option</a></p>
+if($page->getJazzeePage()->getForm()->getElementByName('level')->getValue() > 1) { ?>
+  <p><a href='<?php print $this->controller->getActionPath() ?>'>Choose a different option</a></p>
+<?php } ?>
+
 <div id='leadingText'><?php print $page->getLeadingText()?></div>
 <?php $this->renderElement('form', array('form'=> $page->getJazzeePage()->getForm())); ?>
 <div id='trailingText'><?php print $page->getTrailingText()?></div>
