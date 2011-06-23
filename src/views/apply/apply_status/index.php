21f07c443cf17898afa125f3a1610d8d4b63c37a
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-23 15:42:01 -0700

    dont try and access decisions for incomplete applicants

--- src/views/apply/apply_status/index.php
+++ src/views/apply/apply_status/index.php
@@ -7,17 +7,18 @@
 if(!$applicant->isLocked()){?>
   <h2>Application Status: <em>Not Complete</em></h2>
   <p>Your application was not completed before the application deadline and will not be reviewed.</p>
-<?php }?>
-<?php 
-switch($applicant->getDecision()->status()){
-  case 'finalDeny': print '<h2>Application Status:<em>Denied</em></h2>'; break;
-  case 'finalAdmit': print '<h2>Application Status:<em>Admitted</em></h2>'; break;
-  case 'acceptOffer': print '<h2>Application Status:<em>Accepted</em></h2>'; break;
-  case 'finalDeny': print '<h2>Application Status:<em>Declined</em></h2><p>You have declined our offer of admission or you have missed the deadline for enrollment confirmation.</p>'; break;
-  default: print '<h2>Application Status:<em>Under Review</em></h2>';
-}
-foreach($pages as $page){
-  $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-status-page');
-  $this->renderElement($elementName, array('page'=>$page));
+<?php } else {?>
+  <?php 
+  switch($applicant->getDecision()->status()){
+    case 'finalDeny': print '<h2>Application Status:<em>Denied</em></h2>'; break;
+    case 'finalAdmit': print '<h2>Application Status:<em>Admitted</em></h2>'; break;
+    case 'acceptOffer': print '<h2>Application Status:<em>Accepted</em></h2>'; break;
+    case 'finalDeny': print '<h2>Application Status:<em>Declined</em></h2><p>You have declined our offer of admission or you have missed the deadline for enrollment confirmation.</p>'; break;
+    default: print '<h2>Application Status:<em>Under Review</em></h2>';
+  }
+  foreach($pages as $page){
+    $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-status-page');
+    $this->renderElement($elementName, array('page'=>$page));
+  }
 }
 ?>
