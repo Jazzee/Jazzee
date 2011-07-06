fb4982f2dcd0c99f661bced67c94e9bb1e05d6fe
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-07-06 16:54:43 -0700

    add ETSMatch status page view

---
+++ src/views/page_type_elements/ETSMatch-status-page.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * StandardPage Answer Status Element
+ * @author Jon Johnson <jon.johnson@ucsf.edu>
+ * @license http://jazzee.org/license.txt
+ * @package jazzee
+ * @subpackage apply
+ */
+?>
+<table>
+  <thead>
+    <tr>
+      <th>Scores</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php 
+if($answers = $page->getJazzeePage()->getAnswers()){ ?>
+  <?php foreach($answers as $answer){?>
+    <tr>
+    <td>
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\ETSMatch::FID_TEST_TYPE)->getJazzeeElement()->displayValue($answer);?><br />
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\ETSMatch::FID_REGISTRATION_NUMBER)->getJazzeeElement()->displayValue($answer);?><br />
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\ETSMatch::FID_TEST_DATE)->getJazzeeElement()->displayValue($answer);?>
+    </td>
+    <td>
+    <?php if($answer->getPublicStatus()){?><br />Status: <?php print $answer->getPublicStatus()->getName();?> <br /> <?php }?>
+    Score Status: 
+      <?php if($answer->getMatchedScore()){?>
+        Score received for test taken on <?php print $answer->getMatchedScore()->getTestDate()->format('F jS Y')?>.
+      <?php } else { ?>
+        This score has not been received from ETS.
+      <?php }?>
+    </td>
+  </tr>
+  <?php }
+}
+?>
+  </tbody>
+</table>
