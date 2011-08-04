052191be517f1200a03d09034a46089237ef2b5e
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-08-04 08:55:29 -0700

    add check all to tags list

--- src/scripts/controllers/applicants_list.controller.js
+++ src/scripts/controllers/applicants_list.controller.js
@@ -9,13 +9,25 @@ $(document).ready(function(){
     var title = $('caption', this).text();
     tags.push({id: id, title: title});
   });
-  var ul = $('<ul>');
+  var table = $('<table>');
+  var tr = $('<tr>');
+  tr.append($('<th>').append($('<input>').attr('type', 'checkbox').attr('checked', true).bind('click', function(e){
+    $('#selectors tr>td>input').each(function(){
+      $(this).attr('checked', ($(e.target).is(':checked')));
+    });
+    $('#selectors tr>td>input').trigger('change');
+  })));
+  tr.append($('<th>').html('Tags'));
+  table.append(tr);
+  
   for(var i=0; i < tags.length; i++){
-    var li = $('<li>').html(tags[i].title).prepend($('<input>').attr('type', 'checkbox').attr('checked', true).data('tableId', tags[i].id));
-    ul.append(li);
+    var tr = $('<tr>');
+    tr.append($('<td>').append($('<input>').attr('type', 'checkbox').attr('checked', true).data('tableId', tags[i].id)));
+    tr.append($('<td>').html(tags[i].title));
+    table.append(tr);
   }
-  $('#selectors').append(ul);
-  $('#selectors input').bind('click', function(e){
+  $('#selectors').append(table);
+  $('#selectors tr>td>input').bind('change', function(e){
     if($(e.target).is(':checked')){
       $('#' + $(e.target).data('tableId')).show();
     } else {
