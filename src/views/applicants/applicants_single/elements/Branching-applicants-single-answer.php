d8f62efcf771ec025402446911fb877621295a40
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-08-04 13:13:01 -0700

    branching page display

---
+++ src/views/applicants/applicants_single/elements/Branching-applicants-single-answer.php
@@ -0,0 +1,43 @@
+<?php 
+/**
+ * Applicants single answer
+ */
+?>
+<tr id='answer<?print $answer->getId() ?>'>
+  <?php $child = $answer->getChildren()->first();?>
+  <td><?php print $child->getPage()->getTitle(); ?></td>
+  <td><?php foreach($child->getPage()->getElements() as $element){
+      print '<strong>' . $element->getTitle() . ':</strong>&nbsp;' . $element->getJazzeeElement()->displayValue($child) . '<br />'; 
+  }?></td>
+<td>
+  <strong>Last Updated:</strong> <?php print $answer->getUpdatedAt()->format('M d Y g:i a');?>
+    <?php if($answer->getPublicStatus()){?><br />Public Status: <?php print $answer->getPublicStatus()->getName();}?>
+    <?php if($answer->getPrivateStatus()){?><br />Private Status: <?php print $answer->getPrivateStatus()->getName();}?>
+</td>
+<td>
+<?php if($attachment = $answer->getAttachment()){
+    $blob = $attachment->getAttachment();
+    $name = $answer->getPage()->getTitle() . '_attachment_' . $answer->getId();
+    $pdf = new \Foundation\Virtual\VirtualFile($name . '.pdf', $blob, $answer->getUpdatedAt()->format('c'));
+    $png = new \Foundation\Virtual\VirtualFile($name . '.png', \thumbnailPDF($blob, 100, 0), $answer->getUpdatedAt()->format('c'));
+
+    $session = new \Foundation\Session();
+    $store = $session->getStore('files', 900);
+    $pdfStoreName = md5($name . '.pdf');
+    $pngStoreName = md5($name . '.png');
+    $store->$pdfStoreName = $pdf; 
+    $store->$pngStoreName = $png;
+    ?>
+    <a href="<?php print $this->path('file/' . \urlencode($name . '.pdf'));?>"><img src="<?php print $this->path('file/' . \urlencode($name . '.png'));?>" /></a>
+<?php } ?>
+</td>
+<?php if($this->controller->checkIsAllowed('applicants_single', 'editAnswer')){ ?>
+  <td>
+    <?php if($this->controller->checkIsAllowed('applicants_single', 'editAnswer')){ ?>
+      <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/editAnswer/' . $answer->getId());?>' class='actionForm'>Edit</a><br />     
+    <?php } ?><?php if($this->controller->checkIsAllowed('applicants_single', 'deleteAnswer')){ ?>
+      <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/deleteAnswer/' . $answer->getId());?>' class='action'>Delete</a><br />     
+    <?php } ?>
+  </td>
+<?php }?>
+</tr>
---
+++ src/views/applicants/applicants_single/elements/Branching-applicants-single-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Single branching page
+ */
+$page->getJazzeePage()->setApplicant($applicant);
+?>
+<div id='page<?php print $page->getPage()->getId() ?>'>
+<h4><?php print $page->getTitle(); ?></h4>
+  <div id='answers'>
+    <table class='answer'>
+      <thead>
+        <tr>
+          <th><?php print $page->getPage()->getVar('branchingElementLabel')?></th>
+          <th>Answer</th>
+          <th>Status</th>
+          <th>Attachment</th>
+          <?php if($this->controller->checkIsAllowed('applicants_single', 'editAnswer') or $this->controller->checkIsAllowed('applicants_single', 'deleteAnswer')){ ?><th>Tools</th><?php }?>
+        </tr>
+      </thead>
+      <tbody>
+      <?php 
+      foreach($page->getJazzeePage()->getAnswers() as $answer){
+        $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-applicants-single-answer');
+        $this->renderElement($elementName, array('page'=>$page, 'answer'=>$answer));
+      }?>
+      </tbody>
+    </table>
+ </div><!-- answers -->
+<?php if($this->controller->checkIsAllowed('applicants_single', 'addAnswer') and (is_null($page->getMax()) or count($page->getJazzeePage()->getAnswers()) < $page->getMax())){?>
+    <a class='actionForm' href='<?php print $this->path('applicants/single/' . $applicant->getId() . '/addAnswer/' . $page->getPage()->getId());?>'>Add Answer</a>
+<?php }?>
+</div> <!-- page -->
