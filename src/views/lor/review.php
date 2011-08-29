4efe625f1bdbe0901b71a75ce795d51de49f6b16
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-08-29 09:26:57 -0700

    typo in lor confirmation

--- src/views/lor/review.php
+++ src/views/lor/review.php
@@ -8,7 +8,7 @@
  * @subpackage lor
  */
 ?>
-<h3>Thank you.  We have recieved your recommendation.</h3>
+<h3>Thank you.  We have received your recommendation.</h3>
 <h5>For our applicant's security this page will only display once.  If you wish to have a copy of this recommendation for your records you should print one now.</h5>
 <fieldset>
   <legend>Submitted Recommendation</legend>
