7c77af7308d00dc709215039a0015d16ed31597c
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-09-02 19:58:55 -0700

    add score matching cron to ETSMatch page

--- src/Jazzee/Entity/Page/ETSMatch.php
+++ src/Jazzee/Entity/Page/ETSMatch.php
@@ -115,5 +115,48 @@ class ETSMatch extends Standard {
     $element->setWeight(3);
     $element->setFixedId(self::FID_TEST_DATE);
     $em->persist($element);
-  } 
+  }
+  
+  /**
+   * Match unmatched scores as a cron task
+   * @todo eventually the 1000 limit is going to block on dead scores - find a way to eventually avoid those or to keep looping in 1000 increments
+   * @param AdminCronController $cron
+   */
+  public static function runCron(\AdminCronController $cron){
+    $pageType = $cron->getEntityManager()->getRepository('\Jazzee\Entity\PageType')->findOneBy(array('class'=>'\Jazzee\Entity\Page\ETSMatch'));
+    $allETSMatchPages = $cron->getEntityManager()->getRepository('\Jazzee\Entity\Page')->findBy(array('type'=>$pageType->getId()));
+    foreach($allETSMatchPages as $page){
+      //get all the answers without a matching score.  Limit this to 1000 per cron run to avoid memory time issues run them reverse cronologically so hopefully we don't hit a block with unmatched scores
+      $answers = $cron->getEntityManager()->getRepository('\Jazzee\Entity\Answer')->findBy(array('page'=>$page->getId(), 'greScore'=>null, 'toeflScore'=>null),array('updatedAt'=>'desc'), 1000);
+      foreach($answers as $answer){
+        if(is_null($answer->getGREScore()) and is_null($answer->getTOEFLScore())){
+          $testType = $page->getElementByFixedId(self::FID_TEST_TYPE)->getJazzeeElement()->displayValue($answer);
+          $registrationNumber = $page->getElementByFixedId(self::FID_REGISTRATION_NUMBER)->getJazzeeElement()->displayValue($answer);
+          $testDate = $page->getElementByFixedId(self::FID_TEST_DATE)->getJazzeeElement()->formValue($answer);
+          $testMonth = date('m', strtotime($testDate));
+          $testYear = date('Y', strtotime($testDate));
+          
+          $parameters = array(
+            'registrationNumber' => $registrationNumber,
+            'testMonth' => $testMonth,
+            'testYear' => $testYear
+          );
+          switch($testType){
+            case 'GRE/GRE Subject':
+                $score = $cron->getEntityManager()->getRepository('\Jazzee\Entity\GREScore')->findOneBy($parameters);
+                if($score) $answer->setGreScore($score);
+                $cron->getEntityManager()->persist($answer);
+              break;
+            case 'TOEFL':
+                $score = $cron->getEntityManager()->getRepository('\Jazzee\Entity\TOEFLScore')->findOneBy($parameters);
+                if($score) $answer->setTOEFLScore($score);
+                $cron->getEntityManager()->persist($answer);
+              break;
+            default:
+              throw new \Jazzee\Exception("Unknown test type: {$testType} when trying to match a score");
+          }
+        }
+      }
+    }
+  }
 }
