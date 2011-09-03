6e8144e2c6c76fc41708985f8b2646eba2ca4a6b
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-09-02 22:04:13 -0700

    users can view and update their own api key

---
+++ src/controllers/admin/admin_apikey.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * View and reset API key for a user
+ * @author Jon Johnson <jon.johnson@ucsf.edu>
+ * @package jazzee
+ * @subpackage admin
+ */
+class AdminApikeyController extends \Jazzee\AdminController {
+  const MENU = 'My Account';
+  const TITLE = 'API Key';
+  const PATH = 'apikey';
+  const REQUIRE_AUTHORIZATION = true;
+  const REQUIRE_APPLICATION = false;
+  
+  /**
+   * Display key
+   */
+  public function actionIndex(){
+    $this->setVar('apiKey', $this->_user->getApiKey());
+  }
+  
+  /**
+   * Update the user key
+   */
+  public function actionUpdateKey(){
+    $this->_user->generateApiKey();
+    $this->_em->persist($this->_user);
+    $this->addMessage('success', 'Key updated successfully');
+    $this->redirectPath('apikey');
+  }
+  
+  /**
+   * Only allow access for users with API keys
+   * These keys are generated in the Manage Users menu
+   * @param string $controller
+   * @param string $action
+   * @param \Jazzee\Entity\User $user
+   * @param \Jazzee\Entity\Program $program
+   * @return bool
+   */
+  public static function isAllowed($controller, $action, \Jazzee\Entity\User $user = null, \Jazzee\Entity\Program $program = null, \Jazzee\Entity\Application $application = null){
+    if(in_array($action, array('index', 'updateKey')) AND $user AND $user->getApiKey()){
+      return true;
+    }
+    return parent::isAllowed($controller, $action, $user, $program, $application);
+  }
+}
+?>
---
+++ src/views/admin/admin_apikey/index.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * admin_apikey index view
+ * @package jazzee
+ * @subpackage admin
+ * @subpackage applicants
+ */
+?>
+<p>Your Key: <strong><?php print $apiKey;?></strong></p>
+<?php if($this->controller->checkIsAllowed('admin_apikey', 'updateKey')){?>
+  <a href='<?php print $this->controller->path('apikey/updateKey');?>'>Update Key</a>
+<?php }?>
