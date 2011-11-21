51b80f9a777af5b31c47e23d3a2a166d641c9919
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-11-21 10:47:27 -0800

    add percent complete and tags to the applicant listings in serach and list

--- src/views/applicants/applicants_list/index.php
+++ src/views/applicants/applicants_list/index.php
@@ -12,7 +12,7 @@
     <table id='<?php print strtolower(preg_replace('#[^a-z0-9]#i', '', $title)); ?>'>
     <caption><?php print $title; ?> (<?php print count($applicants); ?>)</caption>
       <thead>
-        <tr><th>View</th><th>Last Name</th><th>First Name</th><th>Last Update</th><th>Progress</th><th>Last Login</th><th>Account Created</th></tr>
+        <tr><th>View</th><th>Last Name</th><th>First Name</th><th>Last Update</th><th>Progress</th><th>Tags</th><th>Last Login</th><th>Account Created</th></tr>
       </thead>
       <tbody>
         <?php foreach($applicants as $applicant){?>
@@ -22,6 +22,20 @@
             <td><?php print $applicant->getFirstName(); ?></td>
             <td><?php print $applicant->getUpdatedAt()->format('m/d/y'); ?></td>
             <td><?php print $applicant->getPercentComplete()*100; ?>%</td>
+            <?php 
+              $tags = array();
+              foreach($applicant->getTags() as $tag){
+                $tags[] = $tag->getTitle();
+              }
+              if($applicant->isLocked()) $tags[] = 'Locked';
+              if($applicant->getDecision() and $applicant->getDecision()->getAcceptOffer()) $tags[] = 'Accepted';
+              if($applicant->getDecision() and $applicant->getDecision()->getFinalAdmit()) $tags[] = 'Admitted';
+              if($applicant->getDecision() and $applicant->getDecision()->getDeclineOffer()) $tags[] = 'Declined';
+              if($applicant->getDecision() and $applicant->getDecision()->getFinalDeny()) $tags[] = 'Denied';
+              asort($tags);
+              $tags = implode(', ', $tags);
+            ?>
+            <td><?php print $tags;?></td>
             <td><?php print $applicant->getLastLogin()->format('m/d/y'); ?></td>
             <td><?php print $applicant->getCreatedAt()->format('m/d/y'); ?></td>
           </tr>
--- src/views/applicants/applicants_search/index.php
+++ src/views/applicants/applicants_search/index.php
@@ -11,21 +11,35 @@ if(isset($applicants)){?>
   <table>
   <caption>Your search returned (<?php print count($applicants); ?>) results</caption>
     <thead>
-      <tr><th>View</th><th>Last Name</th><th>First Name</th><th>Program</th><th>Last Update</th><th>Last Login</th><th>Account Created</th></tr>
-    </thead>
-    <tbody>
-      <?php foreach($applicants as $applicant){?>
-        <tr>
-          <td><a class='applicantLink' programId='<?php print $applicant->getApplication()->getProgram()->getId(); ?>' href='<?php print $this->path('applicants/single/' . $applicant->getId());?>' title='<?php print $applicant->getFirstName() . ' ' . $applicant->getLastName();?>'>Application</a></td>
-          <td><?php print $applicant->getLastName(); ?></td>
-          <td><?php print $applicant->getFirstName(); ?></td>
-          <td><?php print $applicant->getApplication()->getProgram()->getName(); ?></td>
-          <td><?php print $applicant->getUpdatedAt()->format('m/d/y'); ?></td>
-          <td><?php print $applicant->getLastLogin()->format('m/d/y'); ?></td>
-          <td><?php print $applicant->getCreatedAt()->format('m/d/y'); ?></td>
-        </tr>
-      <?php } //applicants?>
-    </tbody>
-  </table>
+      <tr><th>View</th><th>Last Name</th><th>First Name</th><th>Last Update</th><th>Progress</th><th>Tags</th><th>Last Login</th><th>Account Created</th></tr>
+      </thead>
+      <tbody>
+        <?php foreach($applicants as $applicant){?>
+          <tr>
+            <td><a href='<?php print $this->path('applicants/single/' . $applicant->getId());?>' title='<?php print $applicant->getFirstName() . ' ' . $applicant->getLastName();?>'>Application</a></td>
+            <td><?php print $applicant->getLastName(); ?></td>
+            <td><?php print $applicant->getFirstName(); ?></td>
+            <td><?php print $applicant->getUpdatedAt()->format('m/d/y'); ?></td>
+            <td><?php print $applicant->getPercentComplete()*100; ?>%</td>
+            <?php 
+              $tags = array();
+              foreach($applicant->getTags() as $tag){
+                $tags[] = $tag->getTitle();
+              }
+              if($applicant->isLocked()) $tags[] = 'Locked';
+              if($applicant->getDecision() and $applicant->getDecision()->getAcceptOffer()) $tags[] = 'Accepted';
+              if($applicant->getDecision() and $applicant->getDecision()->getFinalAdmit()) $tags[] = 'Admitted';
+              if($applicant->getDecision() and $applicant->getDecision()->getDeclineOffer()) $tags[] = 'Declined';
+              if($applicant->getDecision() and $applicant->getDecision()->getFinalDeny()) $tags[] = 'Denied';
+              asort($tags);
+              $tags = implode(', ', $tags);
+            ?>
+            <td><?php print $tags;?></td>
+            <td><?php print $applicant->getLastLogin()->format('m/d/y'); ?></td>
+            <td><?php print $applicant->getCreatedAt()->format('m/d/y'); ?></td>
+          </tr>
+        <?php } //applicants?>
+      </tbody>
+    </table>
   <?php 
 } //if applicants
