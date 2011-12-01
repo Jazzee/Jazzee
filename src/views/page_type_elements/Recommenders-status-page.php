cef1d5b26eb3b3cba613dc16e68c84ee5a81b899
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-12-01 10:54:53 -0800

    recommenders status page wasnt allowing an invitation to be sent for the first time only resent and wasn't showing a clear indication of the status.  Also added the reocmmender email to the dispay.

--- src/views/page_type_elements/Recommenders-status-page.php
+++ src/views/page_type_elements/Recommenders-status-page.php
@@ -21,20 +21,23 @@ if($answers = $page->getJazzeePage()->getAnswers()){ ?>
     <tr>
     <td>
      <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_FIRST_NAME)->getJazzeeElement()->displayValue($answer);?>
-     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_LAST_NAME)->getJazzeeElement()->displayValue($answer);?>,
-     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_INSTITUTION)->getJazzeeElement()->displayValue($answer);?>    
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_LAST_NAME)->getJazzeeElement()->displayValue($answer);?><br />
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_INSTITUTION)->getJazzeeElement()->displayValue($answer);?><br />
+     <?php print $page->getPage()->getElementByFixedId(\Jazzee\Entity\Page\Recommenders::FID_EMAIL)->getJazzeeElement()->displayValue($answer);?>    
     </td>
     <td>
-    <strong>Last Updated:</strong> <?php print $answer->getUpdatedAt()->format('M d Y g:i a');?><br />
     <?php if($child = $answer->getChildren()->first()){?>
       <strong>Status:</strong> This recommendation was received on <?php print $child->getUpdatedAt()->format('l F jS Y g:ia');
     } else if($answer->isLocked()){?>
+      <strong>Status:</strong> This recommendation had not been received.<br />
       <strong>Invitation Sent:</strong> <?php print $answer->getUpdatedAt()->format('l F jS Y g:ia'); ?>
       <?php if($answer->getUpdatedAt()->diff(new DateTime('now'))->days < \Jazzee\Entity\Page\Recommenders::RECOMMENDATION_EMAIL_WAIT_DAYS){?> 
         You will be able to send the invitation to your recommender again in <?php print (\Jazzee\Entity\Page\Recommenders::RECOMMENDATION_EMAIL_WAIT_DAYS - $answer->getUpdatedAt()->diff(new DateTime('now'))->days);?> days.
       <?php } else {?>
         <br /><a class='invite' href='<?php print $this->controller->getActionPath();?>/do/sendEmail/<?php print $answer->getPage()->getId()?>/<?php print $answer->getId()?>'>Send Reminder Email</a>
       <?php } ?>
+    <?php } else {?>
+      <strong>Status: </strong> No Invitation Sent.  You can <a class='invite' href='<?php print $this->controller->getActionPath();?>/do/sendEmail/<?php print $answer->getPage()->getId()?>/<?php print $answer->getId()?>'>Send the invitation</a>
     <?php }?>
     </td>
   </tr>
