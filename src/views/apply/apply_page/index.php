c08aaed85afed9dc3410c191af9ddb663e0cc717
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-12-19 11:23:39 -0800

    usee controller applyPage method to get the view path

--- src/views/apply/apply_page/index.php
+++ src/views/apply/apply_page/index.php
@@ -16,9 +16,9 @@ $layoutContentTop = "<p class='deadline";
 if($applicant->getApplication()->getClose()->diff(new DateTime('today'))->days < 7){ $layoutContentTop .= ' approaching-deadline';}
 $layoutContentTop .= "'>Application Deadline: " . $deadline->format('m/d/Y g:ia T') . '</p>';
 
-$layoutContentTop .= '<p class="links"><a href="' . $this->path('apply/' . $applicant->getApplication()->getProgram()->getShortName() . '/' . $applicant->getApplication()->getCycle()->getName() . '/support') . '">Support</a>';
+$layoutContentTop .= '<p class="links"><a href="' . $this->controller->applyPath('support') . '">Support</a>';
 if($count = $applicant->unreadMessageCount()) $layoutContentTop .= '<sup class="count">' . $count . '</sup>';
-$layoutContentTop .= '<a href="' . $this->path('apply/' . $applicant->getApplication()->getProgram()->getShortName() . '/' . $applicant->getApplication()->getCycle()->getName() . '/applicant/logout') . '">Log Out</a></p>';
+$layoutContentTop .= '<a href="' . $this->controller->applyPath('/applicant/logout') . '">Log Out</a></p>';
 $this->controller->setLayoutVar('layoutContentTop', $layoutContentTop);    
 $elementName = \Foundation\VC\Config::findElementCacading($page->getPage()->getType()->getClass(), '', '-page');
 $this->renderElement($elementName, array('page'=>$page, 'currentAnswerID'=>$currentAnswerID, 'applicant'=>$applicant));
