cbd1d031c799ff80216f7d254d12c059c5288a8b
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-02-28 15:36:28 -0800

    fix add user to use the directory

--- src/Jazzee/Console/AddUser.php
+++ src/Jazzee/Console/AddUser.php
@@ -14,28 +14,45 @@ class AddUser extends \Symfony\Component\Console\Command\Command
     /**
      * @see Console\Command\Command
      */
-    protected function configure()
-    {
-        $this
-        ->setName('add-user')
-        ->setDescription('Add a new user.')
-        ->setDefinition(array(
-            new \Symfony\Component\Console\Input\InputOption(
-                'uniqueName', null, \Symfony\Component\Console\Input\InputOption::VALUE_REQUIRED,
-                'Uniquename for the new users.  Generally this is their eduPersonPrinciplaName or some other federated authentication name.'
-            )
-        ))
-        ->setHelp('Add a new user.  This will allow the user to log into the system, but until you add them to at least one role they will not be able to do anything.');
+    protected function configure(){
+        $this->setName('add-user')->setDescription('Add a new user.');
+        $this->addArgument('user name', \Symfony\Component\Console\Input\InputArgument::REQUIRED, 'Unser name for the new users.  Use find-user to search for users in the directory.');
+        $this->setHelp('Add a new user.  This will allow the user to log into the system, but until you add them to at least one role they will not be able to do anything.');
     }
     protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output){
-      if(!$input->getOption('uniqueName')){
-        $output->write('<error>--uniqueName is required.</error>' . PHP_EOL);
-        exit(1);
-      }
+      $jazzeeConfiguration = new \Jazzee\Configuration;
       $em = $this->getHelper('em')->getEntityManager();
-      $user = new \Jazzee\Entity\User();
-      $em->persist($user);
-      $user->setUniqueName($input->getOption('uniqueName'));
-      $em->flush();
+      $stub = new AdminStub;
+      $stub = new AdminStub;
+      $stub->em = $em;
+      $stub->config = $jazzeeConfiguration;
+      $class = $jazzeeConfiguration->getAdminDirectoryClass();
+      $directory = new $class($stub);
+      
+      $attributes = array(
+         $jazzeeConfiguration->getLdapUsernameAttribute() => $input->getArgument('user name')
+      );
+      $results = $directory->search($attributes);
+      if(count($results) == 0){
+        $output->write('<error>That user name is not in the directory.</error>' . PHP_EOL);
+      } else if(count($results) > 1){
+        $output->write('<error>This name is not unique in the directory.  It returned ' . count($results) . ' results.</error>' . PHP_EOL);
+        $output->write('<error>The name belongs to:</error>' . PHP_EOL);
+        foreach($results as $arr){
+          $output->write("<error>{$arr['lastName']}, {$arr['firstName']} ({$arr['emailAddress']})</error>" . PHP_EOL);
+        }
+      } else {
+        $arr = $results[0];
+        $user = new \Jazzee\Entity\User();
+        $user->setUniqueName($arr['userName']);
+        $user->setFirstName($arr['firstName']);
+        $user->setLastName($arr['lastName']);
+        $user->setEmail($arr['emailAddress']);
+        $em->persist($user);
+        $em->flush();
+        $output->write("<info>{$arr['lastName']}, {$arr['firstName']} ({$arr['emailAddress']}) add as user #{$user->getId()}</info>" . PHP_EOL);
+      }
+      
+
     }
 }
