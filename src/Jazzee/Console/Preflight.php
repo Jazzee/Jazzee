f7de9aee1c8a12235b60fce268c192715e75fab7
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-03-03 12:47:46 -0800

    cahnges to preflight check.  Even when we are in development run most checks.  Look for HTMLPurifier library (fixes #99).  Better var directory check (fixes #93).

--- src/Jazzee/Console/Preflight.php
+++ src/Jazzee/Console/Preflight.php
@@ -22,10 +22,47 @@ class Preflight extends \Symfony\Component\Console\Command\Command
       $stub->config = $jazzeeConfiguration;
       //Check that the var direcotry is working
       $path = $jazzeeConfiguration->getVarPath()?$jazzeeConfiguration->getVarPath():__DIR__ . '/../../../var';
-      if(!$realPath = \realpath($path) or !\is_dir($realPath) or !\is_writable($realPath)){
+      if(!$realPath = \realpath($path) or !\is_dir($realPath)){
         if($realPath) $path = $realPath; //nicer error message if the path exists
         $error = true;
-        $output->write("<error>{$path} is not writable by the webserver so we cannot use it as the 'var' directory</error>" . PHP_EOL);
+        $output->write("<error>{$path} is does not exist so we cannot use it as the 'var' directory</error>" . PHP_EOL);
+      }
+      $perms = \substr(\sprintf('%o', \fileperms($realPath)), -4);
+      $owner = \fileowner($realPath);
+      $group = \filegroup($realPath);
+      if(function_exists('posix_getpwuid')){
+        $arr = posix_getpwuid($owner);
+        $owner = $arr['name'];
+      }
+      if(function_exists('posix_getgrgid')){
+        $arr = posix_getgrgid($group);
+        $group = $arr['name'];
+      }
+      $output->write("<comment>{$realPath} is owned by {$owner}:{$group} and has permissions {$perms}.  Ensure your webserver can write to that directory</comment>" . PHP_EOL);
+      
+      //Check that the schema is correct
+      $validator = new \Doctrine\ORM\Tools\SchemaValidator($em);
+      $errors = $validator->validateMapping();
+      if($errors) {
+        $error = true;
+        foreach ($errors AS $className => $errorMessages) {
+          $output->write("<error>The entity-class '" . $className . "' mapping is invalid:</error>" . PHP_EOL);
+          foreach ($errorMessages AS $errorMessage) {
+            $output->write('* ' . $errorMessage . PHP_EOL);
+          }
+          $output->write(PHP_EOL);
+        }
+      }
+      if (!$validator->schemaInSyncWithMetadata()) {
+        $error = true;
+        $output->write('<error>The database schema is not correct.  Run "setup update" to fix this.</error>' . PHP_EOL);
+      }
+      try {
+        $em->getConfiguration()->ensureProductionSettings();
+        $em->getConnection()->connect();
+      } catch (\Exception $e) {
+        $error = true;
+        $output->writeln('<error>' . $e->getMessage() . '</error>');
       }
       
       if($jazzeeConfiguration->getStatus() == 'DEVELOPMENT'){
@@ -38,30 +75,6 @@ class Preflight extends \Symfony\Component\Console\Command\Command
           $output->write("<error>APC is required for caching, but it it not installed.</error>" . PHP_EOL);
         }
 
-        //Check that the schema is correct
-        $validator = new \Doctrine\ORM\Tools\SchemaValidator($em);
-        $errors = $validator->validateMapping();
-        if($errors) {
-          $error = true;
-          foreach ($errors AS $className => $errorMessages) {
-            $output->write("<error>The entity-class '" . $className . "' mapping is invalid:</error>" . PHP_EOL);
-            foreach ($errorMessages AS $errorMessage) {
-              $output->write('* ' . $errorMessage . PHP_EOL);
-            }
-            $output->write(PHP_EOL);
-          }
-        }
-        if (!$validator->schemaInSyncWithMetadata()) {
-          $error = true;
-          $output->write('<error>The database schema is not correct.  Run "setup update" to fix this.</error>' . PHP_EOL);
-        }
-        try {
-          $em->getConfiguration()->ensureProductionSettings();
-          $em->getConnection()->connect();
-        } catch (\Exception $e) {
-          $error = true;
-          $output->writeln('<error>' . $e->getMessage() . '</error>');
-        }
 
         $metadatas = $em->getMetadataFactory()->getAllMetadata();
         $path = $em->getConfiguration()->getProxyDir();
@@ -85,31 +98,40 @@ class Preflight extends \Symfony\Component\Console\Command\Command
             $em->getProxyFactory()->generateProxyClasses($metadatas, $proxyDir);          
           }
         }
-        try{
-          //Test the Authentication service
-          $class = $jazzeeConfiguration->getAdminAuthenticationClass();
-          $adminAuthentication = new $class($stub);
-          if(!($adminAuthentication instanceof \Jazzee\Interfaces\AdminAuthentication)){
-            $error = true;
-            $output->write("<error>{$class} does not implement AdminAuthentication Interface.</error>" . PHP_EOL);
-          }
+      }
+      
+      try{
+        //Test the Authentication service
+        $class = $jazzeeConfiguration->getAdminAuthenticationClass();
+        $adminAuthentication = new $class($stub);
+        if(!($adminAuthentication instanceof \Jazzee\Interfaces\AdminAuthentication)){
+          $error = true;
+          $output->write("<error>{$class} does not implement AdminAuthentication Interface.</error>" . PHP_EOL);
+        }
 
-          //Test the directory service
-          $class = $jazzeeConfiguration->getAdminDirectoryClass();
-          $adminDirectory = new $class($stub);
-          if(!($adminDirectory instanceof \Jazzee\Interfaces\AdminDirectory)){
-            $error = true;
-            $output->write("<error>{$class} does not implement AdminDirectory Interface.</error>" . PHP_EOL);
-          }
+        //Test the directory service
+        $class = $jazzeeConfiguration->getAdminDirectoryClass();
+        $adminDirectory = new $class($stub);
+        if(!($adminDirectory instanceof \Jazzee\Interfaces\AdminDirectory)){
+          $error = true;
+          $output->write("<error>{$class} does not implement AdminDirectory Interface.</error>" . PHP_EOL);
+        }
 //          This isn't available until we can limit the search returned Issue #90
 //          $attributes = array(
 //              $jazzeeConfiguration->getLdapLastNameAttribute() => '*'
 //          );
 //          $results = $adminDirectory->search($attributes);
-        } catch (\Jazzee\Exception $e){
-          $error = true;
-          $output->writeln('<error>' . $e->getMessage() . '</error>');
-        }
+      } catch (\Jazzee\Exception $e){
+        $error = true;
+        $output->writeln('<error>' . $e->getMessage() . '</error>');
+      }
+      
+      require_once 'HTMLPurifier.includes.php';
+      require_once 'HTMLPurifier.autoload.php';
+      if(!class_exists('HTMLPurifier')){
+        $error = true;
+        $output->write("<error>HTML Purifier is required and it is not installed.</error>" . PHP_EOL);
+        throw new \Foundation\Exception('');
       }
       
       if($error) $output->write(PHP_EOL . "<error>Preflight Check Failed</error>" . PHP_EOL);
