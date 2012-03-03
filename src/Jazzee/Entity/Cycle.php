357022d81cc6ce10283f510ab5587a9ba1d16d58
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-03-02 16:11:02 -0800

    if a user is not in any programs they can still get the most recent cycle using findBestCycle

--- src/Jazzee/Entity/Cycle.php
+++ src/Jazzee/Entity/Cycle.php
@@ -105,12 +105,17 @@ class CycleRepository extends \Doctrine\ORM\EntityRepository{
    * find best current cycle
    * 
    * If a user doesn't have a cycle we need to search for an find the best current cycle for them
+   * If there is a program then use that, otherwise just get the most recent cycle
    * @param Program $program
    * @return Cycle
    */
-  public function findBestCycle(Program $program){
-    $query = $this->_em->createQuery('SELECT c FROM Jazzee\Entity\Cycle c JOIN c.applications a WHERE a.program = :program ORDER BY c.end DESC');
-    $query->setParameter('program', $program);
+  public function findBestCycle(Program $program = null){
+    if($program){
+      $query = $this->_em->createQuery('SELECT c FROM Jazzee\Entity\Cycle c JOIN c.applications a WHERE a.program = :program ORDER BY c.end DESC');
+      $query->setParameter('program', $program);
+    } else {
+      $query = $this->_em->createQuery('SELECT c FROM Jazzee\Entity\Cycle c ORDER BY c.end DESC');
+    }
     $result = $query->getResult();
     if(count($result)) return $result[0];
     return false;
