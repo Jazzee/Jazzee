a87f74c39ff768a6d8520e57321ce6a0bcb58d92
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-06-18 09:52:50 -0700

    Change the counter for recommenders page to be more clear. Fixes #149

--- src/views/elements/Recommenders-apply_page.php
+++ src/views/elements/Recommenders-apply_page.php
@@ -22,17 +22,16 @@
         <p>
         <?php 
         if(is_null($page->getMax())){
-          if($totalAnswers >= $page->getMin()) print 'You may add as many additional recommenders as you wish to this page, but it is not required.';
-          else print 'You have added ' . $totalAnswers . ' of the ' . $page->getMin() . ' required recommenders on this page.';
+          if($completedAnswers >= $page->getMin()) print 'You may add as many additional recommenders as you wish to this page, but it is not required.';
+          else print 'You have invited ' . $completedAnswers . ' of the ' . $page->getMin() . ' required recommenders on this page.';
         } else {
-          if($page->getMax() - $totalAnswers == 0) print 'You cannot add any more recommenders to this page.';
-          else if($totalAnswers >= $page->getMin()) print 'You may add an additional ' . ($page->getMax() - $totalAnswers) . ' recommenders on this page, but it is not required.';
-          else print 'You have added ' . $totalAnswers . ' of the ' . $page->getMin() . ' required recommenders on this page.';
+          if($page->getMax() - $completedAnswers == 0) print 'You cannot invite any more recommenders to this page.';
+          else if($completedAnswers >= $page->getMin()) print 'You may invite an additional ' . ($page->getMax() - $completedAnswers) . ' recommenders on this page, but it is not required.';
+          else print 'You have invited ' . $completedAnswers . ' of the ' . $page->getMin() . ' required recommenders on this page.';
         }
         ?>
         <?php 
-        if($completedAnswers < $page->getMin() and $completedAnswers < $totalAnswers) print '&nbsp;' . ($page->getMin() - $completedAnswers) . ' recommenders(s) still must be invited to submit their recommendations in order for your application to be complete.';
-        else if($completedAnswers < $totalAnswers) print '&nbsp;You have not invited ' . ($totalAnswers - $completedAnswers) . ' of your recommenders.';
+        if($completedAnswers < $totalAnswers) print '&nbsp;You have not invited ' . ($totalAnswers - $completedAnswers) . ' of the recommenders you added to the page.  Click &lsquo;Send Invitation Email&rsquo; to invite each recommender.';
         ?>
         </p>
       <?php } ?>
