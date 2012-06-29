158a1e7e9dd5942465a5053a26fc92d5047ea3bd
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-06-29 11:51:33 -0700

    Add features to QAS address page min/max and fixed the coutry types box not filling with data.

--- src/scripts/page_types/JazzeePageQASAddress.js
+++ src/scripts/page_types/JazzeePageQASAddress.js
@@ -27,8 +27,42 @@ JazzeePageQASAddress.prototype.workspace = function(){
  * Create the page properties dropdown
 */
 JazzeePageQASAddress.prototype.pageProperties = function(){
+  var pageClass = this;
   var div = $('<div>');
+
+  div.append(this.isRequiredButton());
+
   if(!this.isGlobal || this.pageBuilder.editGlobal) div.append(this.editQASVariablesButton());
+  
+  var slider = $('<div>');
+  slider.slider({
+    value: this.min,
+    min: 0,
+    max: 20,
+    step: 1,
+    slide: function( event, ui ) {
+      pageClass.setProperty('min', ui.value);
+      $('#minValue').html(pageClass.min == 0?'No Minimum':pageClass.min);
+    }
+  });
+  div.append($('<p>').html('Minimum Answers Required ').append($('<span>').attr('id', 'minValue').html(this.min == 0?'No Minimum':this.min)));
+  div.append(slider);
+
+  var slider = $('<div>');
+  slider.slider({
+    value: this.max,
+    min: 0,
+    max: 20,
+    step: 1,
+    slide: function( event, ui ) {
+      pageClass.setProperty('max', ui.value);
+      $('#maxValue').html(pageClass.max == 0?'No Maximum':pageClass.max);
+    }
+  });
+  div.append($('<p>').html('Maximum Answers Allowed ').append($('<span>').attr('id', 'maxValue').html(this.max == 0?'No Maximum':this.max)));
+  div.append(slider);
+
+
   return div;
 };
 
@@ -39,19 +73,19 @@ JazzeePageQASAddress.prototype.editQASVariablesButton = function(){
   var pageClass = this;
   var obj = new FormObject();
   var field = obj.newField({name: 'legend', value: 'Edit QAS Server Address'});
-  
+
   var element = field.newElement('TextInput', 'wsdlAddress');
   element.label = 'Server Address';
   element.required = true;
   element.value = this.getVariable('wsdlAddress');
-  
+
   var element = field.newElement('TextInput', 'validatedCountries');
   element.label = 'List of Countries to Validate';
   element.format = 'USA,GBR';
   element.instructions = 'Comma Seperated list of QAS country codes';
   element.required = true;
-//  element.value = this.getVariable('validatedCountries');
-  
+  element.value = this.getVariable('validatedCountries');
+
   var dialog = this.displayForm(obj);
   $('form', dialog).bind('submit',function(e){
     pageClass.setVariable('wsdlAddress', $('input[name="wsdlAddress"]', this).val());
