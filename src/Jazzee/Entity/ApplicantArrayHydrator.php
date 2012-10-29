02a46308adbae2434e8e341ab93d97288ca376ad
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-10-29 08:41:33 -0700

    When hydrating applicant arrays handle the answer children as well.

--- src/Jazzee/Entity/ApplicantArrayHydrator.php
+++ src/Jazzee/Entity/ApplicantArrayHydrator.php
@@ -16,7 +16,8 @@ class ApplicantArrayHydrator extends \Doctrine\ORM\Internal\Hydration\ArrayHydra
    * @param array $hints
    * @return array
    */
-  public function hydrateAll($stmt, $resultSetMapping, array $hints = array()) {
+  public function hydrateAll($stmt, $resultSetMapping, array $hints = array()) 
+  {
     $result = parent::hydrateAll($stmt, $resultSetMapping, $hints);
     foreach($result as $key => $applicant){
       $answers = $applicant['answers'];
@@ -26,19 +27,42 @@ class ApplicantArrayHydrator extends \Doctrine\ORM\Internal\Hydration\ArrayHydra
         if(!isset($applicant['answers'][$pageId])){
           $applicant['answers'][$pageId] = array();
         }
-        $elements = $answer['elements'];
-        $answer['elements'] = array();
-        foreach($elements as $element){
-          $elementId = $element['element_id'];
-          if(!isset($answer['elements'][$elementId])){
-            $answer['elements'][$elementId] = array();
-          }
-          $answer['elements'][$elementId][] = $element;
-        }
-        $applicant['answers'][$pageId][] = $answer;
+        $applicant['answers'][$pageId][] = $this->hydrateAnswer($answer);
       }
       $result[$key] = $applicant;
     }
+    
     return $result;
+  }
+  
+  /**
+   * Hydrate answer array seperatly so children can be done recursivly
+   * 
+   * @param array $answer
+   * 
+   * @return array
+   */
+  protected function hydrateAnswer(array $answer)
+  {
+    $elements = $answer['elements'];
+    $answer['elements'] = array();
+    foreach($elements as $element){
+      $elementId = $element['element_id'];
+      if(!isset($answer['elements'][$elementId])){
+        $answer['elements'][$elementId] = array();
+      }
+      $answer['elements'][$elementId][] = $element;
+    }
+    if(array_key_exists('children', $answer)){
+      $children = $answer['children'];
+      $answer['children'] = array();
+      foreach($children as $child){
+        $answer['children'][] = $this->hydrateAnswer($child);
+      }
+    } else {
+      $answer['children'] = array();
+    }
+    
+    return $answer;
   }
 }
