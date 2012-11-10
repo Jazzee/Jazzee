3aa00ebd381d207828d684eb8572e58e6b294b16
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-11-09 21:08:44 -0800

    Remove bad /sir string from ApplyStatusController::getActionPath.  It seems to only be used in Recommender_apply-status view.

--- src/controllers/apply/apply_status.php
+++ src/controllers/apply/apply_status.php
@@ -237,7 +237,7 @@ class ApplyStatusController extends \Jazzee\AuthenticatedApplyController
    */
   public function getActionPath()
   {
-    return $this->applyPath('status/sir');
+    return $this->applyPath('status');
   }
 
   /**
