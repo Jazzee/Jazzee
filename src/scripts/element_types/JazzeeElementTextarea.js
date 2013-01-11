eab6582dd011363c132ba8ddc53e811be355959f
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-01-11 10:38:13 -0800

    Allow text area max/min sizes to be entered manually.

--- src/scripts/element_types/JazzeeElementTextarea.js
+++ src/scripts/element_types/JazzeeElementTextarea.js
@@ -18,6 +18,25 @@ JazzeeElementTextarea.prototype.elementProperties = function(){
   var elementClass = this;
   var div = JazzeeElement.prototype.elementProperties.call(this);
   
+  div.append(this.createSlider('min', 'Minimum'));
+  div.append(this.createSlider('max', 'Maximum'));
+  
+  return div;
+};
+
+/**
+ * Seperate out the slider function
+ * @returns {jQuery}
+ */
+JazzeeElementTextarea.prototype.createSlider = function(property, title){
+  var elementClass = this;
+  var div = $('<div>').attr('id', property + 'slider');
+  var link = $('<a>').attr('href', '#').attr('id', property + 'Value').html(elementClass[property]).bind('click', function(){
+    $('#' + property + 'slider').replaceWith(elementClass.createInput(property, title));
+    return false;
+  });
+  div.append($('<p>').html(title + ' Length ').append(link));
+  
   var slider = $('<div>');
   slider.slider({
     value: elementClass.min,
@@ -25,26 +44,32 @@ JazzeeElementTextarea.prototype.elementProperties = function(){
     max: 50000,
     step: 100,
     slide: function( event, ui ) {
-      elementClass.setProperty('min', ui.value);
-      $('#minValue').html(elementClass.min);
+      elementClass.setProperty(property, ui.value);
+      $('#' + property + 'Value').html(elementClass[property]);
     }
   });
-  div.append($('<p>').html('Minimum Length ').append($('<span>').attr('id', 'minValue').html(elementClass.min)));
   div.append(slider);
-  
-  var slider = $('<div>');
-  slider.slider({
-    value: elementClass.max,
-    min: 0,
-    max: 50000,
-    step: 100,
-    slide: function( event, ui ) {
-      elementClass.setProperty('max', ui.value);
-      $('#maxvalue').html(elementClass.max);
-    }
+  return div;
+};
+
+/**
+ * Seeprate out the inptu function
+ * @returns {jQuery}
+ */
+JazzeeElementTextarea.prototype.createInput = function(property, title){
+  var elementClass = this;
+  var div = $('<div>').attr('id', property + 'slider');
+  var link = $('<a>').attr('href', '#').attr('id', property + 'Value').html(elementClass[property]).bind('click', function(){
+    $('#' + property + 'slider').replaceWith(elementClass.createSlider(property, title));
+    return false;
   });
-  div.append($('<p>').html('Maximum Length ').append($('<span>').attr('id', 'maxvalue').html(elementClass.max)));
-  div.append(slider);
+  div.append($('<p>').html(title + ' Length ').append(link));
   
+  var minInput = $('<input>').attr('type', 'text'). attr('size', 5).attr('id', property + 'Input').attr('value', elementClass[property]);
+  minInput.bind('change', function(){
+    elementClass.setProperty(property, $(this).attr('value'));
+    $('#' + property + 'Value').html(elementClass[property]);
+  });
+  div.append(minInput);
   return div;
 };
