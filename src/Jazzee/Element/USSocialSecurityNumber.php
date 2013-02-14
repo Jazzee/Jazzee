d5c42c57097b45e5a25e172e396d125d364149cc
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-02-14 13:36:27 -0800

    Add UC Social Security number Element type.

---
+++ src/Jazzee/Element/USSocialSecurityNumber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Jazzee\Element;
+
+/**
+ * USSocialSecutiryNumber Element
+ * Validate an SSN
+ *
+ * @author  Jon Johnson  <jon.johnson@ucsf.edu>
+ * @license http://jazzee.org/license BSD-3-Clause
+ */
+class USSocialSecurityNumber extends EncryptedTextInput
+{
+
+  const PAGEBUILDER_SCRIPT = 'resource/scripts/element_types/JazzeeElementUSSocialSecurityNumber.js';
+
+  public function addToField(\Foundation\Form\Field $field)
+  {
+    $element = parent::addToField($field);
+    $validator = new \Foundation\Form\Validator\Regex($element, '/^(?!000)([0-6]\d{2}|7([0-6]\d|7[012]))([ -]?)(?!00)\d\d([ -]?)(?!0000)\d{4}$/');
+    $validator->setErrorMessage('This is not a valid US Social Secutiry Number');
+    $element->addValidator($validator);
+    $element->prependFilter(new \Foundation\Form\Filter\Replace($element, array('pattern'=>'/[^0-9]/', 'replace'=>'')));
+    return $element;
+  }
+
+}
---
+++ src/scripts/element_types/JazzeeElementUSSocialSecurityNumber.js
@@ -0,0 +1,23 @@
+/**
+ * The JazzeeElementUSSocialSecurityNumber type
+  @extends JazzeeElement
+ */
+function JazzeeElementUSSocialSecurityNumber(){}
+JazzeeElementUSSocialSecurityNumber.prototype = new JazzeeElement();
+JazzeeElementUSSocialSecurityNumber.prototype.constructor = JazzeeElementUSSocialSecurityNumber;
+
+
+/**
+ * Override JazzeeElement new to set max and format default
+ * @param {String} id the id to use
+ * @returns {JazzeeElementPDFFileInput}
+ */
+JazzeeElementUSSocialSecurityNumber.prototype.newElement = function(id,title,typeId,typeName,typeClass,status,page){
+  var element = JazzeeElementEncryptedTextInput.prototype.newElement.call(this,id,title,typeId,typeName,typeClass,status,page);
+  element.setProperty('format', '');
+  return element;
+};
+
+JazzeeElementUSSocialSecurityNumber.prototype.avatar = function(){
+  return $('<input type="text" disabled="true">');
+};
