cb228e7210ac0d1b7ae2950e0c288050ca9cede2
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-02-25 18:13:16 -0800

    GRE Writing scores are somtimes given as a decimal value, changed all the scoreXConverted data types to use this value;

---
+++ src/Jazzee/Migration/Version20130225000000.php
@@ -0,0 +1,29 @@
+<?php
+namespace Jazzee\Migration;
+
+/**
+ * Update GREScores to use decimals for storing scores
+ */
+class Version20130225000000 extends \Doctrine\DBAL\Migrations\AbstractMigration
+{
+
+  public function up(\Doctrine\DBAL\Schema\Schema $schema)
+  {
+    $table = $schema->getTable('gre_scores');
+    $columns = array('score1converted','score2converted','score3converted','score4converted');
+    foreach($columns as $name){
+      $column = $table->getColumn($name);
+      $column->setType(\Doctrine\DBAL\Types\Type::getType(\Doctrine\DBAL\Types\Type::DECIMAL));
+    }
+  }
+
+  public function down(\Doctrine\DBAL\Schema\Schema $schema)
+  {
+    $table = $schema->getTable('gre_scores');
+    $columns = array('score1converted','score2converted','score3converted','score4converted');
+    foreach($columns as $name){
+      $column = $table->getColumn($name);
+      $column->setType(\Doctrine\DBAL\Types\Type::getType(\Doctrine\DBAL\Types\Type::INTEGER));
+    }
+  }
+}
