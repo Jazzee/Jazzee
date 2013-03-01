c149d7fafe86c5d64b74ae666a88a99855c1275d
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-03-01 08:16:06 -0800

    Make it easy to get at apache log files and run concols commands by putting the puppet user into the apache group.

--- dev/puppet/manifests/web.pp
+++ dev/puppet/manifests/web.pp
@@ -4,9 +4,24 @@ class web {
     centos, redhat: { 
       $enhancers = [ "screen", "emacs", "vim-enhanced" ]
       $firewall = ["iptables", "ip6tables"]
+      file { "/var/log/httpd":
+        ensure => directory,
+        group => 'apache',
+        mode => '0755',
+        require => [Group['apache']]
+      }
+      group { 'apache':
+        ensure => present,
+        require => Class['jazzee']
+      }
+      user { 'vagrant':
+        ensure  => present,
+        groups => ["apache"],
+        require => [Group['apache']]
+      }
     }
   }
-  
+
   package { $enhancers: ensure => "latest" }
 
   service {$firewall: ensure => stopped }
