cf9e1d153f0236df7b24a722a9959f389ebf3fa6
larry <lawrence.roberts@ucsf.edu>
2013-03-19 10:51:03 -0700

    updated lor-confirmation page with recommender name and applicant name.

--- src/views/lor/review.php
+++ src/views/lor/review.php
@@ -5,8 +5,15 @@
  * 
  */
 ?>
-<h3>Thank you.  We have received your recommendation.</h3>
+<h3>Thank you   <?php print $page->getParent()->getElementByFixedId(\Jazzee\Page\Recommenders::FID_FIRST_NAME)->getJazzeeElement()->displayValue($answer); ?>&nbsp;.  We have received your recommendation.</h3>
 <h5>For our applicant's security this page will only display once.  If you wish to have a copy of this recommendation for your records you should print one now.</h5>
+<br/>
+<h5>
+This is a recommendation from <?php print $page->getParent()->getElementByFixedId(\Jazzee\Page\Recommenders::FID_FIRST_NAME)->getJazzeeElement()->displayValue($answer); ?>&nbsp;<?php print $page->getParent()->getElementByFixedId(\Jazzee\Page\Recommenders::FID_LAST_NAME)->getJazzeeElement()->displayValue($answer); ?>
+ for <?php print $applicantFullName; ?>.
+
+</h5>
+<br/><br/>
 <?php
 $class = $page->getType()->getClass();
 $this->renderElement($class::lorReviewElement(), array('page' => $page, 'answer' => $answer));
