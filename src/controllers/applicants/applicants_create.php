b63b93a1a23359c4532783947a12b6aa445cf983
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-05-10 12:55:12 -0700

    Send an email when a new applicant account is created.

--- src/controllers/applicants/applicants_create.php
+++ src/controllers/applicants/applicants_create.php
@@ -55,6 +55,24 @@ class ApplicantsCreateController extends \Jazzee\AdminController
     $element->addValidator(new \Foundation\Form\Validator\EmailAddress($element));
     $element->addFilter(new \Foundation\Form\Filter\Lowercase($element));
 
+    $element = $field->newElement('TextInput', 'notificationSubject');
+    $element->setLabel('Subject for Notification Email');
+    $element->setValue('New Application Account');
+
+    $element = $field->newElement('Textarea', 'notificationMessage');
+    $element->setLabel('Notification Email Message');
+    $element->setFormat('Leave this blank if you do not want to notify the applicant of their new account');
+    
+    $notificationMessagereplacements = array(
+      '_Applicant_Name_',
+      '_Deadline_',
+      '_Link_',
+      '_Email_',
+      '_Password_'
+    );
+    $element->setInstructions('You can use these tokens in the text, they will be replaced automatically: <br />' . implode('</br />', $notificationMessagereplacements));
+    $element->setValue("_Applicant_Name_,\nAn account has been created for you in the {$this->_application->getProgram()->getName()} application.\nYou can login at: _Link_ \nEmail: _Email_\nPassword: _Password_ \nYou have until _Deadline_ to complete your application.");
+    
     $element = $field->newElement('TextInput', 'password');
     $element->setLabel('Password');
     $element->setFormat('If you leave the password blank a random password will be generated.');
@@ -97,6 +115,45 @@ class ApplicantsCreateController extends \Jazzee\AdminController
         $this->setVar('applicant', $applicant);
         $this->setVar('plainTextPassword', $plainTextPassword);
         $this->addMessage('success', 'Applicant Created Successfully');
+        if($input->get('notificationMessage')){
+          $replace = array(
+            $applicant->getFullName(),
+            $applicant->getDeadline()?$applicant->getDeadline()->format('l F jS Y g:ia'):'',
+            $this->absoluteApplyPath("apply/{$this->_application->getProgram()->getShortName()}/{$this->_application->getCycle()->getName()}/applicant/login"),
+            $applicant->getEmail(),
+            $plainTextPassword
+          );
+          $body = str_ireplace($notificationMessagereplacements, $replace, $input->get('notificationMessage'));
+          $subject = $input->get('notificationSubject')?$input->get('notificationSubject'):'New Application Account';
+          $email = $this->newMailMessage();
+          $email->AddCustomHeader('X-Jazzee-Applicant-ID:' . $applicant->getId());
+          $email->AddAddress(
+              $applicant->getFullName(),
+              $applicant->getEmail()
+          );
+          
+          $email->setFrom($this->_application->getContactEmail(), $this->_application->getContactName());
+          $email->Subject = $subject;
+          $email->Body = $body;
+          $email->Send();
+          
+          $thread = new \Jazzee\Entity\Thread();
+          $thread->setSubject($subject);
+          $thread->setApplicant($applicant);
+
+          $message = new \Jazzee\Entity\Message();
+          $message->setSender(\Jazzee\Entity\Message::PROGRAM);
+          $message->setText(nl2br($body));
+          $message->read();
+          $thread->addMessage($message);
+          $this->_em->persist($thread);
+          $this->_em->persist($message);
+          
+         $this->addMessage('success', 'New account email sent to ' . $applicant->getEmail());
+        }
+        
+        
+        
         $form->applyDefaultValues();
       }
     }
