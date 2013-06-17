b282a7a629709c0bd9032d03a7c7eb742843dd8f
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-06-17 15:48:18 -0700

    Fix some vagrant build issues, new puppet labs repository, bad experiences with yum fastestmirror plugin, problems with apache error file permissions.

--- dev/puppet/manifests/web.pp
+++ dev/puppet/manifests/web.pp
@@ -8,7 +8,14 @@ class web {
         ensure => directory,
         group => 'apache',
         mode => '0755',
+        recurse => true, 
         require => [Group['apache']]
+      }
+      file { "/var/log/httpd/error_log":
+        ensure => present,
+        group => 'apache',
+        mode => '0644',
+        require => File['/var/log/httpd']
       }
       group { 'apache':
         ensure => present,
@@ -19,6 +26,10 @@ class web {
         groups => ["apache"],
         require => [Group['apache']]
       }
+      file { "/etc/yum/pluginconf.d/fastestmirror.conf":
+        content => "[main]\nenabled=0"
+      }
+      Package { require => File['/etc/yum/pluginconf.d/fastestmirror.conf'] }
     }
   }
 
