a12b95084b19c2fa7a981022b570bedf230fc585
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-06-19 15:26:59 -0700

    Fix the element list items search by to return a better set of matches.

--- src/Jazzee/Entity/ElementListItemRepository.php
+++ src/Jazzee/Entity/ElementListItemRepository.php
@@ -27,17 +27,24 @@ class ElementListItemRepository extends \Doctrine\ORM\EntityRepository
     $queryBuilder->where('item.element = :elementId');
     $queryBuilder->setParameter('elementId', $element->getId());
     $expression = $queryBuilder->expr()->orX();
-    foreach($searchTerms as $term){
-      $expression->add($queryBuilder->expr()->like("item.value", ":term"));
-      $expression2 = $queryBuilder->expr()->andX();
-      foreach($variables as $key => $name){
-        $expression2->add($queryBuilder->expr()->eq("variable.name", ":v{$key}"));
-        $expression2->add($queryBuilder->expr()->like("variable.value", ":term"));
-        $queryBuilder->setParameter('v'.$key, $name);
-      }
-      $expression->add($expression2);
-      $queryBuilder->setParameter('term', '%'.$term.'%');
+    $expression2 = $queryBuilder->expr()->andX();
+    foreach($searchTerms as $key => $term){
+      $expression2->add($queryBuilder->expr()->like("item.value", ":term{$key}"));
+      $queryBuilder->setParameter("term{$key}", '%'.$term.'%');
     }
+    $expression->add($expression2);
+    $expression2 = $queryBuilder->expr()->andX();
+    foreach($searchTerms as $key => $term){
+      $expression3 = $queryBuilder->expr()->andX();
+      foreach($variables as $key2 => $name){
+        $expression3->add($queryBuilder->expr()->eq("variable.name", ":v{$key2}"));
+        $expression3->add($queryBuilder->expr()->like("variable.value", ":term{$key}"));
+        $queryBuilder->setParameter('v'.$key2, $name);
+      }
+      $expression2->add($expression3);
+      $queryBuilder->setParameter("term{$key}", '%'.$term.'%');
+    }
+    $expression->add($expression2);
     $queryBuilder->andWhere($expression);
     return $queryBuilder->getQuery()->getResult();
   }
