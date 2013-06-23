a1709f48c58962b68b0285879cc0da614cec2515
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-06-22 23:50:00 -0700

    Fix intersection display to use pageId when comparing elements so multiple pages with the same element name can co-exist.

--- src/Jazzee/Display/Intersection.php
+++ src/Jazzee/Display/Intersection.php
@@ -59,7 +59,7 @@ class Intersection implements \Jazzee\Interfaces\Display
           $elementIds[] = $display->getElementIds();
           $ourElements = array();
           foreach($display->listElements() as $element){
-            $ourElements[$element->type.$element->name] = $element;
+            $ourElements[$element->type.$element->name.$element->pageId] = $element;
           }
           $elements[] = $ourElements;
         }
