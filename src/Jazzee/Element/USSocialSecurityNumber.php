1532b5a0c5705f58d24410e0ec4d00ba00871923
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-06-27 10:56:36 -0700

    Catch some common fake SSNs.

--- src/Jazzee/Element/USSocialSecurityNumber.php
+++ src/Jazzee/Element/USSocialSecurityNumber.php
@@ -20,6 +20,19 @@ class USSocialSecurityNumber extends EncryptedTextInput
     $validator = new \Foundation\Form\Validator\Regex($element, '/^(?!000)([0-6]\d{2}|7([0-6]\d|7[012]))([ -]?)(?!00)\d\d([ -]?)(?!0000)\d{4}$/');
     $validator->setErrorMessage('This is not a valid US Social Secutiry Number');
     $element->addValidator($validator);
+    //check if every digit is the same
+    $validator = new \Foundation\Form\Validator\Regex($element, '/^(\d)(?!\1+$)\d*$/');
+    $validator->setErrorMessage('This is not a valid US Social Secutiry Number');
+    $element->addValidator($validator);
+    //check for specific problem cases
+    $problems = array(
+      '123456789',
+      '111223333',
+      '111223456'
+    );
+    $validator = new \Foundation\Form\Validator\Regex($element, '/^((?!.*(' .implode('|', $problems) . ')).*)$/');
+    $validator->setErrorMessage('This is not a valid US Social Secutiry Number bad');
+    $element->addValidator($validator);
     $element->prependFilter(new \Foundation\Form\Filter\Replace($element, array('pattern'=>'/[^0-9]/', 'replace'=>'')));
 
     return $element;
