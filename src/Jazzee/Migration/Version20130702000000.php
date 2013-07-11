94eb9674786f673d01338147ed94282a7b074346
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-07-11 08:16:55 -0700

    Remove a foreign key artifact that should not have been part of the migration.

--- src/Jazzee/Migration/Version20130702000000.php
+++ src/Jazzee/Migration/Version20130702000000.php
@@ -12,17 +12,12 @@ class Version20130702000000 extends \Doctrine\DBAL\Migrations\AbstractMigration
     // Alter table: roles
     $table = $schema->getTable('roles');
     $table->addForeignKeyConstraint('programs', array('program_id'), array('id'), array('onDelete' => 'CASCADE'), 'FK_B63E2EC73EB8070A');
-    $table->removeForeignKey('roles_ibfk_1');
   }
 
   public function down(\Doctrine\DBAL\Schema\Schema $schema)
   {
     // Alter table: roles
     $table = $schema->getTable('roles');
-    $table->addForeignKeyConstraint('programs', array('program_id'), array('id'), array(
-        'onDelete' => NULL,
-        'onUpdate' => NULL,
-    ), 'roles_ibfk_1');
     $table->removeForeignKey('FK_B63E2EC73EB8070A');
   }
 }
