c7067a112deb9760f5ea88019f1dc1a9cbbeef67
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-07-16 11:38:46 -0700

    Check External IDs on create applicants screen.

--- src/views/applicants/applicants_create/bulk.php
+++ src/views/applicants/applicants_create/bulk.php
@@ -12,20 +12,39 @@ if (isset($results)) {  ?>
     <tr><th>Status</th><th>View</th><th>Name</th><th>Email</th><th>Password</th><th>Messages</th></tr>
   </thead><?php
   foreach($results as $arr){
-        $applicant = $arr['applicant'];?>
-        <tr>
-          <td><?php print $arr['status']; ?></td>
-          <td><a class='applicantLink' programId='<?php print $applicant->getApplication()->getProgram()->getId(); ?>' href='<?php print $this->path('applicants/single/' . $applicant->getId()); ?>' title='<?php print $applicant->getFirstName() . ' ' . $applicant->getLastName(); ?>'>Application</a></td>
-          <td><?php print $applicant->getFullName(); ?></td>
-          <td><?php print $applicant->getEmail(); ?></td>
-          <td><?php print $arr['plainTextPassword']; ?></td>
-          <td>
-            <?php foreach($arr['messages'] as $message){
-              print $message . '<br />';
-            }?>
-          </td>
-        </tr>
-<?php  } //if results
+    switch($arr['status']){
+        case 'success':
+        case 'duplicate':
+          $applicant = $arr['applicant'];?>
+          <tr>
+            <td><?php print $arr['status']; ?></td>
+            <td><a class='applicantLink' programId='<?php print $applicant->getApplication()->getProgram()->getId(); ?>' href='<?php print $this->path('applicants/single/' . $applicant->getId()); ?>' title='<?php print $applicant->getFirstName() . ' ' . $applicant->getLastName(); ?>'>Application</a></td>
+            <td><?php print $applicant->getFullName(); ?></td>
+            <td><?php print $applicant->getEmail(); ?></td>
+            <td><?php print $arr['plainTextPassword']; ?></td>
+            <td>
+              <?php foreach($arr['messages'] as $message){
+                print $message . '<br />';
+              }?>
+            </td>
+          </tr><?php
+        break;
+        case 'badExternalId':?>
+          <tr>
+            <td>Bad External ID</td>
+            <td></td>
+            <td><?php print $arr['applicantName']; ?></td>
+            <td><?php print $arr['applicantEmail']; ?></td>
+            <td><?php print $arr['plainTextPassword']; ?></td>
+            <td>
+              <?php foreach($arr['messages'] as $message){
+                print $message . '<br />';
+              }?>
+            </td>
+          </tr><?php
+        break;  
+      } //switch
+  } //if results
 }  //foreach applicants ?>
 
   </tbody>
