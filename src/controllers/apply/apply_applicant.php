0fff03fde7fac1686936855143ca0b483f6eb7ca
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-07-18 10:18:15 -0700

    Fix sp error in password reset email.

--- src/controllers/apply/apply_applicant.php
+++ src/controllers/apply/apply_applicant.php
@@ -118,7 +118,7 @@ class ApplyApplicantController extends \Jazzee\ApplyController
         foreach($applicants as $applicant){
           if($applicant->getApplication()->getId() == $this->_application->getId()){
             $applicant->generateUniqueId();
-            $messageBody = "We have received a request to reset your password.  In order to reset your password you will need to click on the link at the bottom of this email.  This will take you back to the secure website were you will be ale to enter a new password. \n \n"
+            $messageBody = "We have received a request to reset your password.  In order to reset your password you will need to click on the link at the bottom of this email.  This will take you back to the secure website were you will be able to enter a new password. \n \n"
                     . "If you cannot click on the link you should copy and paste it into your browser. \n"
                     . "For your protection this link will only be valid for a limited time. \n \n"
                     . $this->absoluteApplyPath('applicant/resetpassword/' . $applicant->getUniqueId());
