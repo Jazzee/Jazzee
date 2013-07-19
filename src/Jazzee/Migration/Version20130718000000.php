f1e6fc3d9be28bfebb404f0f45b952a080c4676c
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-07-19 14:16:21 -0700

    Fix 20130718 Migration to include fixing some applicants whos school data was lost.

--- src/Jazzee/Migration/Version20130718000000.php
+++ src/Jazzee/Migration/Version20130718000000.php
@@ -12,7 +12,8 @@ class Version20130718000000 extends \Doctrine\DBAL\Migrations\AbstractMigration
     $sql = 'SELECT id from answers WHERE page_id IN ' .
       '(SELECT id from pages where type_id= ' .
       '(SELECT id from page_types where class="\\\Jazzee\\\Page\\\Education")) ' .
-      'AND school_id IS NOT NULL';
+      'AND school_id IS NOT NULL ' .
+      'AND id IN (SELECT parent_id FROM answers)';
     $rows = $this->connection->executeQuery($sql)->fetchAll();
     $fixAnswer = $this->connection->prepare('DELETE FROM answers WHERE parent_id = ? AND id NOT IN (SELECT answer_id from element_answers)');
     $count = 0;
@@ -20,7 +21,31 @@ class Version20130718000000 extends \Doctrine\DBAL\Migrations\AbstractMigration
       $fixAnswer->execute(array($row['id']));
       $count++;
     }
-    $this->write("<info>Removed {$count} answers for Education pages where a bug caused an extra child answer to be stored.</info>");
+    if($count > 0){
+      $this->write("<info>Removed {$count} answers for Education pages where a bug caused an extra child answer to be stored.</info>");
+    }
+
+    $sql = 'SELECT id, page_id from answers WHERE parent_id IN ' . 
+      '(SELECT id from answers WHERE page_id IN ' .
+      '(SELECT id from pages where type_id= ' .
+      '(SELECT id from page_types where class="\\\Jazzee\\\Page\\\Education")) ' .
+      'AND school_id IS NULL) AND id NOT IN (SELECT answer_id FROM element_answers)';
+    $rows = $this->connection->executeQuery($sql)->fetchAll();
+    $statement = 'INSERT INTO element_answers (answer_id, element_id, position, eShortString) VALUES ' .
+    '(:answerId, (SELECT id FROM elements WHERE page_id = :pageId AND fixedId=' . \Jazzee\Page\Education::ELEMENT_FID_NAME . '), 0, "Unknown School"), ' .
+    '(:answerId, (SELECT id FROM elements WHERE page_id = :pageId AND fixedId=' . \Jazzee\Page\Education::ELEMENT_FID_COUNTRY . '), 0, "Unknown"), ' .
+    '(:answerId, (SELECT id FROM elements WHERE page_id = :pageId AND fixedId=' . \Jazzee\Page\Education::ELEMENT_FID_CITY . '), 0, "Unknown")';
+    $addUnknowSchool = $this->connection->prepare($statement);
+    $count = 0;
+    foreach($rows as $row){
+      $addUnknowSchool->bindValue(':answerId', $row['id'],  \PDO::PARAM_INT);
+      $addUnknowSchool->bindValue(':pageId', $row['page_id'], \PDO::PARAM_INT);
+      $addUnknowSchool->execute();
+      $count++;
+    }
+    if($count > 0){
+      $this->write("<info>Modified {$count} answers for Education pages where a bug caused Known School data to be dropped.</info>");
+    }
   }
   
   public function down(\Doctrine\DBAL\Schema\Schema $schema){}
