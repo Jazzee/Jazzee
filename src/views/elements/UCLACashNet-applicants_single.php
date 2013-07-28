43649390888119fad29ea0bd7275d4c51a523eec
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-07-27 23:38:03 -0700

    Clean up cashnet single applicant view payment display.

--- src/views/elements/UCLACashNet-applicants_single.php
+++ src/views/elements/UCLACashNet-applicants_single.php
@@ -2,73 +2,56 @@
 /**
  * Single applicant AuthorizeNetPayment view
  */
+$fields = array();
+$tools = array();
+$fields['Amount'] = $answer->getPayment()->getAmount();
 ?>
 <fieldset id='answer<? print $answer->getId() ?>'>
-  <legend><?php print $answer->getPayment()->getType()->getName(); ?> Payment</legend>
-  <?php
+  <legend><?php print $answer->getPayment()->getType()->getName(); ?> Payment</legend><?php
   switch ($answer->getPayment()->getStatus()) {
-    case \Jazzee\Entity\Payment::PENDING:?>
-      <p>
-        <strong>Amount:</strong>&nbsp;$<?php print $answer->getPayment()->getAmount(); ?><br />
-        Status: Pending Settlement<br />
-        Applicant Status Message: Approved<br />
-        UCLA Reference Number: <?php print $answer->getPayment()->getVar('UCLA_REF_NO'); ?><br />
-        Payment Type: <?php print $answer->getPayment()->getVar('pmttype'); ?><br />
-        Transaction Number: <?php print $answer->getPayment()->getVar('tx'); ?>
-      </p>
-      <div class='tools'>
-        <?php
-        if ($this->controller->checkIsAllowed('applicants_single', 'settlePayment')) { ?>
-          <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/settlePayment/' . $answer->getId()); ?>' class='actionForm'>Settle Payment</a><?php
-        }
-        if ($this->controller->checkIsAllowed('applicants_single', 'rejectPayment')) { ?>
-          <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/rejectPayment/' . $answer->getId()); ?>' class='actionForm'>Void Payment</a><?php
-        }
-        if ($this->controller->checkIsAllowed('applicants_single', 'refundPayment')) { ?>
-          <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/refundPayment/' . $answer->getId()); ?>' class='actionForm'>Refund Payment</a><?php
-        } ?>
-      </div><?php
-        break;
-    case \Jazzee\Entity\Payment::SETTLED:?>
-      <p>
-        <strong>Amount:</strong>&nbsp;$<?php print $answer->getPayment()->getAmount(); ?><br />
-        Status: Marked as Settled<br />
-        Applicant Status Message: Approved<br />
-        UCLA Reference Number: <?php print $answer->getPayment()->getVar('UCLA_REF_NO'); ?><br />
-        Payment Type: <?php print $answer->getPayment()->getVar('pmttype'); ?><br />
-        Payment Type: <?php print $answer->getPayment()->getVar('pmttype'); ?><br />
-        Transaction Number: <?php print $answer->getPayment()->getVar('tx'); ?>
-      </p>
-      <div class='tools'>
-        <?php
-        if ($this->controller->checkIsAllowed('applicants_single', 'rejectPayment')) { ?>
-          <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/rejectPayment/' . $answer->getId()); ?>' class='actionForm'>Void Payment</a><?php
-        }
-        if ($this->controller->checkIsAllowed('applicants_single', 'refundPayment')) { ?>
-          <a href='<?php print $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/refundPayment/' . $answer->getId()); ?>' class='actionForm'>Refund Payment</a><?php
-        } ?>
-      </div><?php
-        break;
-  case \Jazzee\Entity\Payment::REFUNDED:?>
-    <p>
-      <strong>Amount:</strong>&nbsp;$<?php print $answer->getPayment()->getAmount(); ?><br />
-      Status: Marked as Refunded<br />
-      Applicant Status Message: <?php print $answer->getPayment()->getVar('refundedReason'); ?><br />
-      UCLA Reference Number: <?php print $answer->getPayment()->getVar('UCLA_REF_NO'); ?><br />
-        Payment Type: <?php print $answer->getPayment()->getVar('pmttype'); ?><br />
-        Transaction Number: <?php print $answer->getPayment()->getVar('tx'); ?>
-    </p><?php
+    case \Jazzee\Entity\Payment::PENDING:
+      $fields['Status'] = 'Pending Settlement';
+      $fields['Applicant Status Message'] = 'Approved';
+      if ($this->controller->checkIsAllowed('applicants_single', 'settlePayment')) {
+        $tools['Settle Payment'] = $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/settlePayment/' . $answer->getId());
+      }
+      if ($this->controller->checkIsAllowed('applicants_single', 'rejectPayment')) {
+        $tools['Reject Payment'] = $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/rejectPayment/' . $answer->getId());
+      }
+      break;
+    case \Jazzee\Entity\Payment::SETTLED:
+      $fields['Status'] = 'Settled';
+      $fields['Applicant Status Message'] = 'Approved';
+      if ($this->controller->checkIsAllowed('applicants_single', 'refundPayment')) {
+        $tools['Refund Payment'] = $this->path('applicants/single/' . $answer->getApplicant()->getId() . '/refundPayment/' . $answer->getId());
+      }
+      break;
+    case \Jazzee\Entity\Payment::REFUNDED:
+      $fields['Status'] = 'Refunded';
+      $fields['Applicant Status Message'] = $answer->getPayment()->getVar('refundedReason');
+      $fields['Net Amount'] = '$' . ($answer->getPayment()->getAmount() + $answer->getPayment()->getVar('refundAmount'));
+      $fields['Refund Transaction Number'] = $answer->getPayment()->getVar('refundTransactionId');
+      $fields['Refund Amount'] = $answer->getPayment()->getVar('refundAmount');
     break;
-  case \Jazzee\Entity\Payment::REJECTED:?>
-    <p>
-      <strong>Amount:</strong>&nbsp;$<?php print $answer->getPayment()->getAmount(); ?><br />
-      Status: Marked as Voided<br />
-      Applicant Status Message: <?php print $answer->getPayment()->getVar('rejectedReason'); ?><br />
-      UCLA Reference Number: <?php print $answer->getPayment()->getVar('UCLA_REF_NO'); ?><br />
-        Payment Type: <?php print $answer->getPayment()->getVar('pmttype'); ?><br />
-        Transaction Number: <?php print $answer->getPayment()->getVar('tx'); ?>
-    </p><?php
+    case \Jazzee\Entity\Payment::REJECTED:
+      $fields['Status'] = 'Marked as Voided';
+      $fields['Applicant Status Message'] = $answer->getPayment()->getVar('rejectedReason');
     break;
   }
+  $fields['UCLA Reference Number'] = $answer->getPayment()->getVar('UCLA_REF_NO');
+  $fields['Transaction Number'] = $answer->getPayment()->getVar('tx');
+  $fields['Customer Code'] = $answer->getPayment()->getVar('custcode');
+  $fields['Payment Code'] = $answer->getPayment()->getVar('pmtcode');
+  $fields['Item Code'] = $answer->getPayment()->getVar('itemcode');?>
+  <p><?php
+  foreach($fields as $title => $value){
+    print $title . ': ' . $value . '<br />';
+  }?>
+  </p>
+  <div class='tools'><?php 
+  foreach($tools as $title => $link){?>
+    <a href='<?php print $link; ?>' class='actionForm'><?php print $title; ?></a><?php
+  }?>
+  </div><?php
 ?>
 </fieldset>
