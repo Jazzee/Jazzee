df4afc54643fa2988df48c18fe82f09252c2d49b
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-08-04 17:44:02 -0700

    Remove fastest mirror during basebox build, update and restart and use the latest kernel.

--- dev/definitions/Jazzee_CentOS64/base.sh
+++ dev/definitions/Jazzee_CentOS64/base.sh
@@ -2,6 +2,11 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
+cat > /etc/yum/pluginconf.d/fastestmirror.conf << EOM
+[main]
+enabled=0
+EOM
+
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
@@ -10,5 +15,7 @@ enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils
+yum -y update
+yum -y install gcc make gcc-c++ kernel-devel zlib-devel openssl-devel readline-devel sqlite-devel perl wget nfs-utils
 
+service sshd stop && reboot
