bbafbfad94e2008c5d06b91299818ac6826b5f02
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-09-20 15:20:17 -0700

    Ensure PageIds are part of elements when created by JS page classes, otherwise the display cannot be saved.

--- src/scripts/page_types/JazzeePageStandard.js
+++ src/scripts/page_types/JazzeePageStandard.js
@@ -243,8 +243,9 @@ JazzeePageStandard.prototype.editDisplaySortElementButton = function(){
  */
 JazzeePageStandard.prototype.listDisplayElements = function(){
   var elements = [];
+  var self = this;
   $(this.elements).each(function(){
-    elements.push({name: this.id, title: this.title, type: 'element'});
+    elements.push({name: this.id, title: this.title, type: 'element', pageId: self.id});
   });
 
   elements.push({name: 'attachment', type: 'page', title: this.title + ' Attachment', pageId: this.id});
