dcb970c03a1e4fbe2699f406c67e88b22431a3e0
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-09-26 14:53:59 -0700

    Split applicant bio into multiple columns

--- src/views/payments/payments_report/index.php
+++ src/views/payments/payments_report/index.php
@@ -9,18 +9,18 @@ $this->renderElement('form', array('form' => $form));
 if ($payments) {?>
   <table id ='payments'>
     <caption><?php print $searchResultsDescription; ?>  </caption>
-    <thead><tr><th>Payment Type</th><th>Last Update</th><th>Applicant</th><th>Program</th><th>Cycle</th><th>Notes</th><th>Amount</th></tr></thead>
+    <thead><tr><th>Payment Type</th><th>Last Update</th><th>ID</th><th>Last Name</th><th>First Name</th><th>Program</th><th>Cycle</th><th>Notes</th><th>Amount</th></tr></thead>
     <tbody><?php
       foreach ($payments as $payment) { ?>
         <tr>
           <td><?php print $payment['type']['name']; ?></td>
           <td><?php print $payment['updatedAt']->format('c'); ?></td>
           <td applicantId='<?php print $payment['applicantId']; ?>' 
-                    programId='<?php print $payment['programId']; ?>'>
-              <?php print $payment['applicantFirstName'] ?> 
-              <?php print $payment['applicantLastName'] ?>
-              (#<?php print $payment['applicantId'] ?>)
+              programId='<?php print $payment['programId']; ?>'>
+              <?php print $payment['applicantId'] ?>
           </td>
+          <td><?php print $payment['applicantLastName']; ?></td>
+          <td><?php print $payment['applicantFirstName']; ?></td>
           <td><?php print $payment['programName']; ?></td>
           <td><?php print $payment['cycleName']; ?></td>
           <td><?php foreach($payment['notes'] as $title => $value){?>
