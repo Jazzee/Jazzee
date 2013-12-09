829f5f87a649dffc1610698353801770d8561a00
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-12-09 08:58:24 -0800

    Add some clarifying instructions to previews Give users some informatio about browser sessions so they understand how to use previews correctly, especially when sending LOR/message links.

--- src/views/setup/setup_previewapplication/index.php
+++ src/views/setup/setup_previewapplication/index.php
@@ -3,6 +3,19 @@
  * setup_previewapplication index view
  *
  */
+?>
+<p>Previews rely on your browser remembering that it is in preview mode to work correctly.  If you close your browser or
+switch to a different window this can cause problems with the preview system.  This is not a problem when follwing links
+within the application, however if you are testing functionality that relies on receiving a link via email that 
+link may not work correctly unless the following steps are taken:
+<ol>
+    <li>Copy the link sent by the application from your email client</li>
+    <li>Click the preview link below to put your browser in preview mode.  If you have multiple preview links you must
+    use the same one you used when sending the email link</li>
+    <li>In the same browser window and tab paste in the link you received in your email</li>
+</ol>
+</p>
+<?php
 if (!empty($existing)) {?>
   <h5>Existing Application Previews:</h5>
   <ul><?php
